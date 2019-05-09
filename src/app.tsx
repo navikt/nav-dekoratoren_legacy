b356ba1712367761f72e7fb2fa052ebd2c26c039
Terese Haug <terese.haug@nav.no>
2019-05-09 11:23:24 +0200

    IN-1081 Oppdaterer package.json og småendrnger i index og app

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,9 @@
 import * as React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-
-const test = () : boolean => {
-
-    return true;
-};
+import Header from './komponenter/header/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header/>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import '@babel/polyfill'
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
+import './index.less';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('app-fragments'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
