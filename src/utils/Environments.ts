424f96496d798066b95c82bc5cdb1643ceb088c9
havardmj <Havard.Myrvold.Johannessen@nav.no>
2019-08-22 09:32:18 +0200

    fikset feil i server oppsettet

--- src/utils/Environments.ts
+++ src/utils/Environments.ts
@@ -1,5 +1,3 @@
-const env = window.location.origin;
-
 const Environments = () => {
     if (process.env.NODE_ENV === 'development') {
         return {
@@ -7,7 +5,8 @@ const Environments = () => {
             baseUrl: 'http://localhost:3000',
             loginUrl: '#',
             logoutUrl: '#',
-            menypunkter: `${env}/person/nav-dekoratoren/api/get/menyvalg`,
+            menypunkter:
+                'http://localhost:8088/person/nav-dekoratoren/api/get/menyvalg',
         };
     }
     if (window.location.hostname.indexOf('www-q0') > -1) {
@@ -16,7 +15,7 @@ const Environments = () => {
             baseUrl: 'https://www-q0.nav.no',
             loginUrl: 'https://loginservice-q.nav.no',
             logoutUrl: 'https://loginservice-q.nav.no/slo',
-            menypunkter: `${env}/person/nav-dekoratoren/api/get/menyvalg`,
+            menypunkter: '/person/nav-dekoratoren/api/get/menyvalg',
         };
     }
     if (window.location.hostname.indexOf('www-q1') > -1) {
@@ -25,17 +24,16 @@ const Environments = () => {
             baseUrl: 'https://www-q1.nav.no',
             loginUrl: 'https://loginservice-q.nav.no',
             logoutUrl: 'https://loginservice-q.nav.no/slo',
-            menypunkter: `${env}/person/nav-dekoratoren/api/get/menyvalg`,
+            menypunkter: '/person/nav-dekoratoren/api/get/menyvalg',
         };
     }
-    // TODO: før dekoratør skal settes i produksjon må url punkter til prod oppdateres, -
-    //  kan ikke gjøres før enonic xp er i produksjon
+
     return {
         miljo: 'PROD',
         baseUrl: 'https://www.nav.no',
         loginUrl: 'https://loginservice.nav.no',
         logoutUrl: 'https://loginservice.nav.no/slo',
-        menypunkter: `${env}/person/nav-dekoratoren/api/get/menyvalg`,
+        menypunkter: '/person/nav-dekoratoren/api/get/menyvalg',
     };
 };
 
