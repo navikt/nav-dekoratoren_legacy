55b3ba53ae17bbd47730f02e8e2bf6b7dc5a9fbd
Terese Haug <terese.haug@nav.no>
2019-08-23 15:13:45 +0200

    Refaktorerer Toppmeny. Flytter state inn i komponenten

--- src/komponenter/header/HeaderContent.tsx
+++ src/komponenter/header/HeaderContent.tsx
@@ -1,63 +1,25 @@
 import * as React from 'react';
-import {
-    hentStatus,
-    MenuValue,
-    NAVHEADER,
-} from '../../provider/Storage-provider';
 import Skiplinks from './skiplinks/Skiplinks';
 import Toppmeny from './toppmeny/Toppmeny';
 import Hovedmeny from './hovedmeny/Hovedmeny';
 import './Header.less';
 
-interface State {
-    toppmeny: MenuValue;
-}
-
-class HeaderContent extends React.Component<{}, State> {
-
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            toppmeny: hentStatus(),
-        };
-    }
-
-    render() {
-        return (
-            <>
-                <Skiplinks />
-                <div id="header-withmenu">
-                    <div className="hodefot">
-                        <header className="siteheader blokk-m">
-                            <div className="innhold-container">
-                                <Toppmeny
-                                    menyValg={this.state.toppmeny}
-                                    callMenuStorage={this.setMenuStorage}
-                                />
-                                <Hovedmeny />
-                            </div>
-                        </header>
-                    </div>
+const HeaderContent = () => {
+    return (
+        <>
+            <Skiplinks />
+            <div id="header-withmenu">
+                <div className="hodefot">
+                    <header className="siteheader blokk-m">
+                        <div className="innhold-container">
+                            <Toppmeny />
+                            <Hovedmeny />
+                        </div>
+                    </header>
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    );
+};
 
-    private setMenuStorage = (
-        e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>,
-        valgVerdi: MenuValue,
-        url: string
-    ): void => {
-        e.preventDefault();
-        const headervalg = sessionStorage.getItem(NAVHEADER);
-        if (headervalg && headervalg === valgVerdi) {
-            return;
-        }
-        sessionStorage.setItem(NAVHEADER, valgVerdi);
-        this.setState({
-            toppmeny: valgVerdi,
-        });
-        window.location.href = url;
-    };
-}
 export default HeaderContent;
--- src/komponenter/header/toppmeny/Toppmeny.tsx
+++ src/komponenter/header/toppmeny/Toppmeny.tsx
@@ -1,64 +1,87 @@
-import * as React from 'react';
+import React from 'react';
 import { EtikettLiten } from 'nav-frontend-typografi';
 import BEMHelper from '../../../utils/bem';
-import './Toppmeny.less';
 import { toppmenyLenker } from './Toppmeny-lenker';
-import { MenuValue } from '../../../provider/Storage-provider';
+import {
+    hentStatus,
+    MenuValue,
+    NAVHEADER,
+} from '../../../provider/Storage-provider';
+import './Toppmeny.less';
 
-const cls = BEMHelper('toppmeny');
-
-interface Props {
-    menyValg: MenuValue;
-    callMenuStorage: (
-        e: React.MouseEvent<HTMLAnchorElement>,
-        valgVerdi: MenuValue,
-        url: string
-    ) => void;
+interface State {
+    toppmeny: MenuValue;
 }
 
-const Toppmeny = (props: Props) => {
-    return (
-        <nav className="toppmeny">
-            <ul className={cls.element('topp-liste-rad')} role="tablist">
-                {toppmenyLenker.map(
-                    (lenke: {
-                        tittel: string;
-                        url: string;
-                        key: MenuValue;
-                    }) => {
-                        return (
-                            <li
-                                role="tab"
-                                aria-selected={props.menyValg === lenke.tittel ? 'true' : 'false'}
-                                className={cls.element('list-element')}
-                                key={lenke.tittel}
-                            >
-                                <a
-                                    className={cls.element('lenke')}
-                                    href={lenke.url}
-                                    onClick={event =>
-                                        props.callMenuStorage(
-                                            event,
-                                            lenke.key,
-                                            lenke.url
-                                        )
-                                    }
-                                >
-                                    <div
-                                        className={cls.element('inner', props.menyValg === lenke.tittel ? 'active' : '')}
-                                    >
-                                        <EtikettLiten>
-                                            {lenke.tittel}
-                                        </EtikettLiten>
-                                    </div>
-                                </a>
-                            </li>
-                        );
-                    }
-                )}
-            </ul>
-        </nav>
-    );
-};
+class Toppmeny extends React.Component<{}, State> {
 
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            toppmeny: hentStatus(),
+        };
+    }
+
+    render() {
+        const cls = BEMHelper('toppmeny');
+
+        return (
+            <nav className="toppmeny">
+                <ul className={cls.element('topp-liste-rad')} role="tablist">
+                    {toppmenyLenker.map(
+                        (lenke: {
+                            tittel: string;
+                            url: string;
+                            key: MenuValue;
+                        }) => {
+                            return (
+                                <li
+                                    role="tab"
+                                    aria-selected={this.state.toppmeny === lenke.tittel ? 'true' : 'false'}
+                                    className={cls.element('list-element')}
+                                    key={lenke.tittel}
+                                >
+                                    <a
+                                        className={cls.element('lenke')}
+                                        href={lenke.url}
+                                        onClick={event =>
+                                            this.setMenuStorage(
+                                                event,
+                                                lenke.key,
+                                                lenke.url
+                                            )
+                                        }
+                                    >
+                                        <div className={cls.element('inner', this.state.toppmeny === lenke.tittel ? 'active' : '')}>
+                                            <EtikettLiten>
+                                                {lenke.tittel}
+                                            </EtikettLiten>
+                                        </div>
+                                    </a>
+                                </li>
+                            );
+                        }
+                    )}
+                </ul>
+            </nav>
+        );
+    }
+
+    private setMenuStorage = (
+        e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>,
+        valgVerdi: MenuValue,
+        url: string
+    ): void => {
+        e.preventDefault();
+        const headervalg = sessionStorage.getItem(NAVHEADER);
+        if (headervalg && headervalg === valgVerdi) {
+            return;
+        }
+        sessionStorage.setItem(NAVHEADER, valgVerdi);
+        this.setState({
+                          toppmeny: valgVerdi,
+                      });
+        window.location.href = url;
+    };
+}
 export default Toppmeny;
