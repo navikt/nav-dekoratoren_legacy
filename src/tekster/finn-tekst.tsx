eb0c24cf48240bd2c4832a444bb3e1a5b1444c4e
Terese Haug <terese.haug@nav.no>
2019-09-12 13:38:24 +0200

    Språkvelger. Fjerner unødvendig kode

--- src/tekster/finn-tekst.tsx
+++ src/tekster/finn-tekst.tsx
@@ -6,14 +6,13 @@ import { Language } from '../reducer/language-duck';
 
 export function utledTekst(id: string, language: Language): string {
     let ledetekst: string = ledetekster[id];
-    let id2 = id;
 
     if (language === Language.ENGELSK) {
-        id2 += '-en';
-        ledetekst = ledetekster[id2];
+        id += '-en';
+        ledetekst = ledetekster[id];
     } else if (language === Language.SAMISK) {
-        id2 += '-se';
-        ledetekst = ledetekster[id2];
+        id += '-se';
+        ledetekst = ledetekster[id];
     }
     if (!ledetekst) {
         console.error(`Kunne ikke finne teksten ${id}! Returnerer oppgitt id.`); // tslint:disable-line:no-console
