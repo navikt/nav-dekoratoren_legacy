aa714261f903b379fb7bc0b1ec623b6ab491428c
Terese Haug <terese.haug@nav.no>
2019-10-09 10:22:19 +0200

    Endrer key på menygruppe/venstre-seksjon

--- src/komponenter/header/hovedmeny/dropdown-meny/Dropdown-venstre-seksjon.tsx
+++ src/komponenter/header/hovedmeny/dropdown-meny/Dropdown-venstre-seksjon.tsx
@@ -18,37 +18,36 @@ const DropdownVenstreSeksjon = (props: Props) => {
 
     return (
         <div className={cls.element('hovedSeksjon')}>
-            {menyLenker.children.map((menygruppe: MenySeksjon) => {
-                return (
-                    <section
-                        className={cls.element('seksjon')}
-                        key={menygruppe.displayName}
-                    >
-                        <div className={cls.element('seksjonOverskrift')}>
-                            <MediaQuery maxWidth={1023}>
-                                <Ekspanderbartpanel
-                                    tittel={menygruppe.displayName}
-                                    tittelProps="normaltekst"
-                                    border
-                                >
+            {menyLenker.children.map(
+                (menygruppe: MenySeksjon, index: number) => {
+                    return (
+                        <section className={cls.element('seksjon')} key={index}>
+                            <div className={cls.element('seksjonOverskrift')}>
+                                <MediaQuery maxWidth={1023}>
+                                    <Ekspanderbartpanel
+                                        tittel={menygruppe.displayName}
+                                        tittelProps="normaltekst"
+                                        border
+                                    >
+                                        <Menylenker
+                                            menygruppe={menygruppe}
+                                            tabindex={tabindex}
+                                        />
+                                    </Ekspanderbartpanel>
+                                </MediaQuery>
+
+                                <MediaQuery minWidth={1024}>
+                                    <Element>{menygruppe.displayName}</Element>
                                     <Menylenker
                                         menygruppe={menygruppe}
                                         tabindex={tabindex}
                                     />
-                                </Ekspanderbartpanel>
-                            </MediaQuery>
-
-                            <MediaQuery minWidth={1024}>
-                                <Element>{menygruppe.displayName}</Element>
-                                <Menylenker
-                                    menygruppe={menygruppe}
-                                    tabindex={tabindex}
-                                />
-                            </MediaQuery>
-                        </div>
-                    </section>
-                );
-            })}
+                                </MediaQuery>
+                            </div>
+                        </section>
+                    );
+                }
+            )}
         </div>
     );
 };
