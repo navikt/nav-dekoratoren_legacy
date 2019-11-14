a6bfb940712d645bdaf72e1f44f0944df4f08c81
Tobias McVey <tobias.mcvey@nav.no>
2019-11-14 12:46:01 +0100

    add react-gtm-module to render script

--- src/index.tsx
+++ src/index.tsx
@@ -16,6 +16,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import './index.less';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+    gtmId: 'GTM-PM9RP3',
+    dataLayerName: 'datalayer'
+}
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(<App />, document.getElementById('decorator-header'));
 
--- src/server/ssr-server.tsx
+++ src/server/ssr-server.tsx
@@ -10,13 +10,6 @@ import { Provider as ReduxProvider } from 'react-redux';
 import Footer from '../komponenter/footer/Footer';
 import getStore from './../redux/store';
 import Head from '../Head';
-import TagManager from 'react-gtm-module';
-
-const tagManagerArgs = {
-    gtmId: 'GTM-PM9RP3',
-    dataLayerName: 'datalayer'
-}
-TagManager.initialize(tagManagerArgs)
 
 const app = express();
 const PORT = 8088;
