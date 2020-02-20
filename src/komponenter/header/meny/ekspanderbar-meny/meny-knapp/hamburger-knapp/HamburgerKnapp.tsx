6fb4ed70c9c97df6de8713d7b63956d644401844
anders-nom <anders@nomerstad.com>
2020-02-20 10:47:12 +0100

    Ny hamburgerknapp med animasjon

--- src/komponenter/header/meny/ekspanderbar-meny/meny-knapp/hamburger-knapp/HamburgerKnapp.tsx
+++ src/komponenter/header/meny/ekspanderbar-meny/meny-knapp/hamburger-knapp/HamburgerKnapp.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './HamburgerKnapp.less';
+import BEMHelper from '../../../../../../utils/bem';
 
 type Props = {
     isOpen: boolean,
     className?: string
 }
 
+const Linje = ({className, isOpen}: {className: string, isOpen: boolean}) => (
+    <span className={`${className}${isOpen ? ` ${className}--open` : ''}`} />);
+
 const HamburgerKnapp = ({isOpen, className}: Props) => {
+    const cls = BEMHelper('hamburger-knapp');
+
     return (
-        <div className={`hamburger-knapp-container${className ? ` ${className}` : ''}`}>
-            <div className={`hamburger-knapp${isOpen ? '--open' : ''}`} />
+        <div className={`${cls.element('container')}${className ? ` ${className}` : ''}`}>
+            <Linje className={cls.element('topp')} isOpen={isOpen} />
+            <Linje className={cls.element('midt')} isOpen={isOpen} />
+            <Linje className={cls.element('bunn')} isOpen={isOpen} />
         </div>
     );
 };
