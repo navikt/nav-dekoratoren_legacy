deb5f5180f6a3b72bbcae398290f687ee6894e69
Morten Jansrud <morten.jansrud@nav.no>
2020-02-26 16:05:08 +0100

    Oppdater jest

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
     setupTestFrameworkScriptFile: '<rootDir>/setupTest.js',
 
     moduleNameMapper: {
-        '\\.(css|jpg|png|svg|less)$': '<rootDir>/styleMock.js',
-        'nav-(.*)-style': '<rootDir>/styleMock.js',
+        '\\.(css|jpg|png|svg|less)$': '<rootDir>/src/styleMock.js',
+        'nav-(.*)-style': '<rootDir>/src/styleMock.js',
     },
 
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
