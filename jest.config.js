3a8fc91e476705ae9262737018f124117fa6ae67
Morten Jansrud <morten.jansrud@nav.no>
2020-02-26 16:05:45 +0100

    Fiks path: src -> src/utils

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
     setupTestFrameworkScriptFile: '<rootDir>/setupTest.js',
 
     moduleNameMapper: {
-        '\\.(css|jpg|png|svg|less)$': '<rootDir>/src/styleMock.js',
-        'nav-(.*)-style': '<rootDir>/src/styleMock.js',
+        '\\.(css|jpg|png|svg|less)$': '<rootDir>/src/utils/styleMock.js',
+        'nav-(.*)-style': '<rootDir>/src/utils/styleMock.js',
     },
 
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
