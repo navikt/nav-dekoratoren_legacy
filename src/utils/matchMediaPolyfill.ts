5948e032e1bf943fcbe6c4e11ec292ad79bd9e37
anders-nom <anders@nomerstad.com>
2020-02-26 15:16:29 +0100

    Fikser matchMedia polyfill for browsere der MediaQueryList ikke er global (Safari++)

--- src/utils/matchMediaPolyfill.ts
+++ src/utils/matchMediaPolyfill.ts
@@ -1,22 +1,29 @@
+import { verifyWindowObj } from './Environment';
+
 const noMatchMediaSupportFallback = {
-    addEventListener: (_: string, __: () => void) => null,
-    removeEventListener: (_: string, __: () => void) => null,
+    addEventListener: (_: string, __: (e: any) => void) => null,
+    removeEventListener: (_: string, __: (e: any) => void) => null,
 };
 
 export const matchMediaPolyfill = (mediaQuery: string) => {
     // @ts-ignore
-    if (!window.matchMedia) {
+    if (!verifyWindowObj() || !window.matchMedia) {
         return noMatchMediaSupportFallback;
     }
 
-    if (!MediaQueryList.prototype.addEventListener) {
-        const mql = window.matchMedia(mediaQuery);
+    const mql = window.matchMedia(mediaQuery);
+
+    if (!mql.addEventListener) {
         // @ts-ignore
-        mql.addEventListener = (_: string, callback: () => void) =>
-            mql.addListener(callback);
+        mql.addEventListener = (
+            _: string,
+            callback: (e: MediaQueryListEvent) => void
+        ) => mql.addListener(callback);
         // @ts-ignore
-        mql.removeEventListener = (_: string, callback: () => void) =>
-            mql.removeListener(callback);
+        mql.removeEventListener = (
+            _: string,
+            callback: (e: MediaQueryListEvent) => void
+        ) => mql.removeListener(callback);
         return mql;
     }
 
