4c390400cffd363b6186592323d4e7315ea03cf8
anders-nom <anders@nomerstad.com>
2020-03-05 17:01:21 +0100

    - Oppdatert test for minside-meny

--- src/komponenter/header/meny/ekspanderende-menyer/minside-meny-desktop/MinsideMenyDesktop.test.tsx
+++ src/komponenter/header/meny/ekspanderende-menyer/minside-meny-desktop/MinsideMenyDesktop.test.tsx
@@ -4,9 +4,37 @@ import { MinsideMenyDesktop } from './MinsideMenyDesktop';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { reducer } from '../../../../../reducer/reducer';
-import { settPersonflate } from '../../../../../reducer/arbeidsflate-duck';
+import { settArbeidsgiverflate, settPersonflate, settSamarbeidspartnerflate } from '../../../../../reducer/arbeidsflate-duck';
+import { ActionType } from '../../../../../redux/actions';
+import { Status } from '../../../../../api/api';
+import mockMenu from '../../../../../server/mock/menu.json';
 
 const store = createStore(reducer);
+store.dispatch({
+    type: ActionType.HENT_MENY_OK,
+    status: Status.OK,
+    data: mockMenu
+});
+
+const innloggetAction = {
+    type: ActionType.HENT_INNLOGGINGSSTATUS_OK,
+    status: Status.OK,
+    data: {
+        authenticated: true,
+        name: 'Ola Nordmann',
+        securityLevel: '4',
+    }
+};
+
+const uInnloggetAction = {
+    type: ActionType.HENT_INNLOGGINGSSTATUS_OK,
+    data: {
+        authenticated: false,
+        name: '',
+        securityLevel: '',
+    },
+    status: Status.IKKE_STARTET,
+};
 
 const mountWithRedux = () => {
     return mount(
@@ -16,28 +44,32 @@ const mountWithRedux = () => {
     );
 };
 
-// TODO: lage nye passende tester for denne komponenten
 describe('<MinsideMenyDesktop>', () => {
-    it('Skal ikke vise minsidelenke når bruker er PRIVATPERSON og uinnlogget', () => {
+    it('Skal ikke vise minside knapp når bruker er PRIVATPERSON og uinnlogget', () => {
         store.dispatch(settPersonflate());
+        store.dispatch(uInnloggetAction);
         const wrapper = mountWithRedux();
-        expect(wrapper.find('#desktop-minside-meny__knapp')).toHaveLength(0);
+        expect(wrapper.find('.desktop-minside-meny__knapp')).toHaveLength(0);
     });
 
-    // it('Skal vise minsidelenke når bruker er PRIVATPERSON og innlogget', () => {
-    //     store.dispatch(settPersonflate());
-    //     store.dispatch();
-    //     const wrapper = mountWithRedux();
-    //     expect(wrapper.find('#desktop-minside-meny__knapp')).toHaveLength(1);
-    // });
-    //
-    // it('Skal ikke vise minsidelenke når bruker er SAMARBEIDSPARTNER og innlogget', () => {
-    //     const wrapper = mountWithRedux(true, MenuValue.SAMARBEIDSPARTNER);
-    //     expect(wrapper.find('.lenke')).toHaveLength(0);
-    // });
+    it('Skal vise minside knapp når bruker er PRIVATPERSON og innlogget', () => {
+        store.dispatch(settPersonflate());
+        store.dispatch(innloggetAction);
+        const wrapper = mountWithRedux();
+        expect(wrapper.find('.desktop-minside-meny__knapp')).toHaveLength(1);
+    });
 
-    // it('Skal vise riktig tabindex', () => {
-    //     const wrapper = mountWithRedux(true, MenuValue.ARBEIDSGIVER);
-    //     expect(wrapper.find('a[tabindex="0"]')).toBeTruthy();
-    // });
+    it('Skal ikke vise minside knapp når bruker er SAMARBEIDSPARTNER og innlogget', () => {
+        store.dispatch(settSamarbeidspartnerflate());
+        store.dispatch(innloggetAction);
+        const wrapper = mountWithRedux();
+        expect(wrapper.find('.desktop-minside-meny__knapp')).toHaveLength(0);
+    });
+
+    it('Skal vise riktig tabindex', () => {
+        store.dispatch(settArbeidsgiverflate());
+        store.dispatch(innloggetAction);
+        const wrapper = mountWithRedux();
+        expect(wrapper.find('a[tabindex="0"]')).toBeTruthy();
+    });
 });
