5cdeba74e1881ff5119753cf656243b944e03e24
Morten Jansrud <morten.jansrud@nav.no>
2020-03-11 11:03:49 +0100

    Flytt knappene inn i søkefeltet

--- src/komponenter/header/meny/sok/sok-innhold/DesktopSokknapp.tsx
+++ src/komponenter/header/meny/sok/sok-innhold/DesktopSokknapp.tsx
@@ -1,32 +1,25 @@
 import React from 'react';
-import Knapp from 'nav-frontend-knapper/lib/knapp';
 import Tekst from '../../../../../tekster/finn-tekst';
 import BEMHelper from '../../../../../utils/bem';
 
 const DesktopSokknapp = () => {
-    const cls = BEMHelper('sok-meny-ikon');
+    const cls = BEMHelper('sok-knapp');
     return (
         <div className="sok-knapper">
-            <div className="sok-knapp sok-knapp-avbryt">
-                <Knapp type="flat" onClick={() => console.log('Lol')}>
-                    <div className={cls.element('container')}>
-                        <div className={cls.element('circle')} />
-                        <div className={cls.element('line')} />
-                        <div className={cls.element('line-x')} />
-                    </div>
-                    <Tekst id="sok-reset" />
-                </Knapp>
-            </div>
-            <div className="sok-knapp sok-knapp-submit">
-                <Knapp type="flat" htmlType="submit">
-                    <div className={cls.element('container')}>
-                        <div className={cls.element('circle')} />
-                        <div className={cls.element('line')} />
-                        <div className={cls.element('line-x')} />
-                    </div>
-                    <Tekst id="sok-knapp" />
-                </Knapp>
-            </div>
+            <button className="sok-knapp sok-knapp-avbryt">
+                <div className={cls.element('container')}>
+                    <div className={cls.element('reset-line-x')} />
+                    <div className={cls.element('reset-line-y')} />
+                </div>
+                <Tekst id="sok-reset" />
+            </button>
+            <button className="sok-knapp sok-knapp-submit" type="submit">
+                <div className={cls.element('container')}>
+                    <div className={cls.element('sok-circle')} />
+                    <div className={cls.element('sok-line')} />
+                </div>
+                <Tekst id="sok-knapp" />
+            </button>
         </div>
     );
 };
