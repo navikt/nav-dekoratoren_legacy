874f72d40308f977b5fee430adef6498275cddd5
Morten Jansrud <morten.jansrud@nav.no>
2020-03-26 10:49:49 +0100

    Fjern unøvendig logging

--- src/komponenter/header/Header.tsx
+++ src/komponenter/header/Header.tsx
@@ -1,12 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchMenypunkter } from '../../reducer/menu-duck';
 import Skiplinks from './skiplinks/Skiplinks';
 import MenyBakgrunn from './meny/ekspanderende-menyer/meny-bakgrunn/MenyBakgrunn';
-import {
-    oppdaterSessionStorage,
-    MenuValue,
-} from '../../utils/meny-storage-utils';
+import { MenuValue } from '../../utils/meny-storage-utils';
+import { oppdaterSessionStorage } from '../../utils/meny-storage-utils';
 import Environment from '../../utils/Environment';
 import { SimpleHeader } from './HeaderSimple';
 import { RegularHeader } from './HeaderRegular';
@@ -21,10 +19,8 @@ export const Header = () => {
         }
     }, []);
 
-    console.log(Environment.SIMPLE);
-
     return (
-        <>
+        <Fragment>
             <div className="header-z-wrapper">
                 <Skiplinks />
             </div>
@@ -32,7 +28,7 @@ export const Header = () => {
                 {Environment.SIMPLE ? <SimpleHeader /> : <RegularHeader />}
             </header>
             <MenyBakgrunn />
-        </>
+        </Fragment>
     );
 };
 
