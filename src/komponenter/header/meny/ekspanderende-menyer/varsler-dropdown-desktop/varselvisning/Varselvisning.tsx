18a1f0d74a08cd393721fa85bd4d23f5b11e129b
Morten Jansrud <morten.jansrud@nav.no>
2020-04-03 10:44:53 +0200

    Fiks API_VARSELINNBOKS_URL

--- src/komponenter/header/meny/ekspanderende-menyer/varsler-dropdown-desktop/varselvisning/Varselvisning.tsx
+++ src/komponenter/header/meny/ekspanderende-menyer/varsler-dropdown-desktop/varselvisning/Varselvisning.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { AppState } from '../../../../../../reducer/reducer';
-import Environment from '../../../../../../utils/Environment';
+import { AppState } from '../../../../../../reducer/reducers';
 import './Varselvisning.less';
 import { GACategory } from '../../../../../../utils/google-analytics';
 import Tekst, { finnTekst } from '../../../../../../tekster/finn-tekst';
@@ -22,23 +21,28 @@ const stateSelector = (state: AppState) => ({
 
 const classname = 'varsler-display-desktop';
 
-const alleVarslerLenke = (index: number, nyeVarslerMsg: string) => (
-    <div className="dekorator-vis-alle-lenke">
-        <LenkeMedGA
-            href={Environment.API_VARSELINNBOKS_URL}
-            id={getKbId(NaviGroup.Varsler, { col: 0, row: index, sub: 0 })}
-            tabIndex={0}
-            gaEventArgs={{
-                category: GACategory.Header,
-                action: 'varsler/visalle',
-                label: Environment.API_VARSELINNBOKS_URL,
-            }}
-        >
-            <Tekst id={'varsler-visalle'} />
-            {nyeVarslerMsg}
-        </LenkeMedGA>
-    </div>
-);
+const alleVarslerLenke = (index: number, nyeVarslerMsg: string) => {
+    const { API_VARSELINNBOKS_URL } = useSelector(
+        (state: AppState) => state.environment
+    );
+    return (
+        <div className="dekorator-vis-alle-lenke">
+            <LenkeMedGA
+                href={API_VARSELINNBOKS_URL}
+                id={getKbId(NaviGroup.Varsler, { col: 0, row: index, sub: 0 })}
+                tabIndex={0}
+                gaEventArgs={{
+                    category: GACategory.Header,
+                    action: 'varsler/visalle',
+                    label: API_VARSELINNBOKS_URL,
+                }}
+            >
+                <Tekst id={'varsler-visalle'} />
+                {nyeVarslerMsg}
+            </LenkeMedGA>
+        </div>
+    );
+};
 
 export const Varselvisning = () => {
     const { varsler, varslerAntall, varslerUleste, language } = useSelector(
