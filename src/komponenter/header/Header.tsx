dd022610d54e1e2f73d93deea82daff4819d07e2
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-04-15 21:02:42 +0200

    lagt til stickyheader paa scroll

--- src/komponenter/header/Header.tsx
+++ src/komponenter/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, Fragment, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMenypunkter } from 'store/reducers/menu-duck';
 import Skiplinks from './skiplinks/Skiplinks';
@@ -14,27 +14,56 @@ export const Header = () => {
     const { PARAMS, APP_BASE_URL } = useSelector(
         (state: AppState) => state.environment
     );
+    const [headeroffsetHeight, setHeaderoffsetHeight] = useState<
+        number | undefined
+    >(undefined);
+
+    const setMinHeight = () => {
+        const headwrapper = document.getElementById('head-wrapper');
+        setHeaderoffsetHeight(headwrapper ? headwrapper.offsetHeight : 201);
+    };
 
     useEffect(() => {
         fetchMenypunkter(APP_BASE_URL)(dispatch);
         if (PARAMS.CONTEXT !== MenuValue.IKKEVALGT) {
             oppdaterSessionStorage(PARAMS.CONTEXT);
         }
+        setMinHeight();
+        const header = document.getElementById('stickyhead');
+        window.onscroll = function stickHeader() {
+            setClassList(header);
+        };
     }, []);
+
+    const setClassList = (element: HTMLElement | null) => {
+        if (element) {
+            window.pageYOffset > element.offsetTop
+                ? element.classList.add('sticky')
+                : element.classList.remove('sticky');
+        }
+    };
 
     return (
         <Fragment>
-            <div className="header-z-wrapper">
-                <Skiplinks />
+            <div
+                className="head-wrapper"
+                style={{ minHeight: headeroffsetHeight }}
+                id="head-wrapper"
+            >
+                <div className="head-container sticky" id="stickyhead">
+                    <div className="header-z-wrapper">
+                        <Skiplinks />
+                    </div>
+                    <header className="siteheader">
+                        {PARAMS.SIMPLE || PARAMS.SIMPLE_HEADER ? (
+                            <SimpleHeader />
+                        ) : (
+                            <RegularHeader />
+                        )}
+                    </header>
+                    <MenyBakgrunn />
+                </div>
             </div>
-            <header className="siteheader">
-                {PARAMS.SIMPLE || PARAMS.SIMPLE_HEADER ? (
-                    <SimpleHeader />
-                ) : (
-                    <RegularHeader />
-                )}
-            </header>
-            <MenyBakgrunn />
         </Fragment>
     );
 };
