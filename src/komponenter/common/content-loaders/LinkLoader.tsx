75ee9483e98578f178a84e5c1bf6461b4c2c7841
Morten Jansrud <morten.jansrud@nav.no>
2020-04-15 16:26:36 +0200

    Change loader-background

--- src/komponenter/common/content-loaders/LinkLoader.tsx
+++ src/komponenter/common/content-loaders/LinkLoader.tsx
@@ -11,8 +11,8 @@ export const LinksLoader = ({ id }: { id: string }) => (
     <ContentLoader
         speed={1}
         uniqueKey={id}
-        backgroundColor={'white'}
-        foregroundColor={'lightgray'}
+        backgroundColor={'#19548a'}
+        foregroundColor={'#196da2'}
     >
         <rect x="0" y="5" rx="4" ry="4" width="100" height="10" />
         <rect x="0" y="50" rx="4" ry="4" width="135" height="10" />
