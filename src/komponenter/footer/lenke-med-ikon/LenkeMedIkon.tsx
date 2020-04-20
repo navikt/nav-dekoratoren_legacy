155f818ec8830fce9c6dc5ba87dc54d4e1c3c21f
Odd Einar Hoel <odd.einar.hoel@nav.no>
2020-04-20 22:57:23 +0200

    Refactor LenkeMedIkon komponent

--- src/komponenter/footer/lenke-med-ikon/LenkeMedIkon.tsx
+++ src/komponenter/footer/lenke-med-ikon/LenkeMedIkon.tsx
@@ -25,21 +25,17 @@ const LenkeMedIkon: React.StatelessComponent<Props> = ({
 }) => {
     const cls = BEMHelper('lenke-med-ikon');
     return (
-        <div
+        <Lenke
             className={classnames(className, cls.className, {
                 [cls.modifier('venstrestiltIkon')]: venstrestiltIkon,
             })}
+            onClick={onClick}
+            href={href}
+            role="button"
         >
-            <Lenke
-                className={cls.element('lenke')}
-                onClick={onClick}
-                href={href}
-                role="button"
-            >
-                {tekst}
-            </Lenke>
-            <span className={cls.element('ikon')}>{ikon}</span>
-        </div>
+            <span>{tekst}</span>
+            <div>{ikon}</div>
+        </Lenke>
     );
 };
 export default LenkeMedIkon;
