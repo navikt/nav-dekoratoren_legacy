270ed0c99f7bbc6c9f3cb40df05b55dba6df6171
Odd Einar Hoel <odd.einar.hoel@nav.no>
2020-04-20 23:01:38 +0200

    Cleanup ikoner

--- src/ikoner/del-skjerm/DelSkjerm.tsx
+++ src/ikoner/del-skjerm/DelSkjerm.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
+import React, { SVGProps } from 'react';
 
-interface Props {
-    height: number;
-    width: number;
-}
-const DelSkjerm = ({ height, width }: Props) => (
+const DelSkjerm = (props: SVGProps<SVGSVGElement>) => (
     <svg
         viewBox="0 0 24 24"
-        height={height}
-        width={width}
         preserveAspectRatio="xMidYMid meet"
         xmlns="http://www.w3.org/2000/svg"
         version="1.0"
+        {...props}
     >
         <g
             fill="none"
