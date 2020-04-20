86f7475a30163a0d127bd720ee6b3c1795604136
Odd Einar Hoel <odd.einar.hoel@nav.no>
2020-04-21 00:26:30 +0200

    Fix icon spacing

--- src/komponenter/footer/footer-regular/footer-topp/FooterTopp.tsx
+++ src/komponenter/footer/footer-regular/footer-topp/FooterTopp.tsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Undertittel } from 'nav-frontend-typografi';
-import Lenke from 'nav-frontend-lenker';
-import BEMHelper from 'utils/bem';
+
 import Tekst from 'tekster/finn-tekst';
+import PilOppHvit from 'ikoner/meny/PilOppHvit';
+import LenkeMedIkon from 'komponenter/footer/lenke-med-ikon/LenkeMedIkon';
+import { AppState } from 'store/reducers';
+import { MenyNode } from 'store/reducers/menu-duck';
+import { findNode, getLanguageNode } from 'utils/meny-storage-utils';
+import BEMHelper from 'utils/bem';
+
 import Spraakvalg from './spraakvalg/Spraakvalg';
 import FooterArbeidsflatevalg from './footer-arbeidsflatevalg/FooterArbeidsflatevalg';
-import PilOppHvit from 'ikoner/meny/PilOppHvit';
-import { useSelector } from 'react-redux';
-import { AppState } from 'store/reducers';
-import { findNode, getLanguageNode } from 'utils/meny-storage-utils';
-import { MenyNode } from 'store/reducers/menu-duck';
 import FooterLenker from '../../Lenker';
-import './FooterTopp.less';
 import { LinksLoader } from '../../../common/content-loaders/LinkLoader';
-import LenkeMedIkon from 'komponenter/footer/lenke-med-ikon/LenkeMedIkon';
+
+import './FooterTopp.less';
 
 const FooterTopp = () => {
     const cls = BEMHelper('menylinje-topp');
@@ -47,7 +49,11 @@ const FooterTopp = () => {
                             href="#scroll-til-toppen"
                             onClick={scrollToTop}
                             tekst={<Tekst id="footer-til-toppen" />}
-                            ikon={<PilOppHvit />}
+                            ikon={
+                                <PilOppHvit
+                                    style={{ height: '18px', width: '18px' }}
+                                />
+                            }
                             venstrestiltIkon={true}
                         />
                     </div>
