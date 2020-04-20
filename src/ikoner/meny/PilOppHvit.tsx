6b103421c9e5e16854a4493e61af0e6883e0d465
Odd Einar Hoel <odd.einar.hoel@nav.no>
2020-04-20 23:44:34 +0200

    Delete rendundant css attributes

--- src/ikoner/meny/PilOppHvit.tsx
+++ src/ikoner/meny/PilOppHvit.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { SVGProps } from 'react';
 
-const PilOppHvit = () => {
+const PilOppHvit = (props: SVGProps<SVGSVGElement>) => {
     return (
         <svg
             width="18px"
@@ -12,6 +12,7 @@ const PilOppHvit = () => {
             xmlns="http://www.w3.org/2000/svg"
             x="0px"
             y="0px"
+            {...props}
         >
             <path
                 fill="#ffffff"
