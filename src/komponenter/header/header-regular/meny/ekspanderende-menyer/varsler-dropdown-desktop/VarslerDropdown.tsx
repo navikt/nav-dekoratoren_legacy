cd25f7788c39440814e2390b88b6f76e61f5b5f3
anders <anders@nomerstad.com>
2020-04-23 15:58:00 +0200

    Pynter på imports

--- src/komponenter/header/header-regular/meny/ekspanderende-menyer/varsler-dropdown-desktop/VarslerDropdown.tsx
+++ src/komponenter/header/header-regular/meny/ekspanderende-menyer/varsler-dropdown-desktop/VarslerDropdown.tsx
@@ -3,6 +3,7 @@ import { AppState } from 'store/reducers';
 import { useDispatch, useSelector } from 'react-redux';
 import EkspanderbarMeny from '../ekspanderbar-meny/EkspanderbarMeny';
 import { toggleVarsler } from 'store/reducers/dropdown-toggle-duck';
+import { settVarslerSomLest } from 'store/reducers/varsel-lest-duck';
 import { Normaltekst } from 'nav-frontend-typografi';
 import Tekst from 'tekster/finn-tekst';
 import { GACategory, triggerGaEvent } from 'utils/google-analytics';
@@ -11,23 +12,26 @@ import { VarselIkon } from '../meny-knapper/ikoner/varsel-ikon/VarselIkon';
 import { Varselvisning } from './varselvisning/Varselvisning';
 import { MenuValue } from 'utils/meny-storage-utils';
 import './VarslerDropdown.less';
-import { settVarslerSomLest } from '../../../../../../store/reducers/varsel-lest-duck';
 
 const stateSelector = (state: AppState) => ({
     isOpen: state.dropdownToggles.varsler,
     varsler: state.varsler.data,
     innloggetStatus: state.innloggingsstatus.data,
     arbeidsflate: state.arbeidsflate.status,
-    appBaseUrl: state.environment.APP_BASE_URL
+    appBaseUrl: state.environment.APP_BASE_URL,
 });
 
 const classname = 'desktop-varsler-dropdown';
 export const desktopVarslerKnappId = `${classname}-knapp-id`;
 
 export const VarslerDropdown = () => {
-    const { isOpen, varsler, innloggetStatus, arbeidsflate, appBaseUrl } = useSelector(
-        stateSelector
-    );
+    const {
+        isOpen,
+        varsler,
+        innloggetStatus,
+        arbeidsflate,
+        appBaseUrl,
+    } = useSelector(stateSelector);
     const dispatch = useDispatch();
 
     if (
