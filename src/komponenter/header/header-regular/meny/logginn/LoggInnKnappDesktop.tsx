32962eb08777e7f4f894aae56c5f0e28daa91214
anders <anders@nomerstad.com>
2020-04-27 20:08:08 +0200

    Bugfix

--- src/komponenter/header/header-regular/meny/logginn/LoggInnKnappDesktop.tsx
+++ src/komponenter/header/header-regular/meny/logginn/LoggInnKnappDesktop.tsx
@@ -3,17 +3,15 @@ import React from 'react';
 import { LoggInnKnappProps } from 'komponenter/header/header-regular/meny/logginn/LoggInn';
 import LoggInn from 'komponenter/header/header-regular/meny/logginn/LoggInn';
 
-export const LoggInnKnappDesktop = () => {
-    const knapp = ({ handleButtonClick, tekst }: LoggInnKnappProps) => (
-        <KnappBase
-            type="standard"
-            className="login-knapp"
-            id="desktop-login-knapp"
-            onClick={handleButtonClick}
-        >
-            {tekst}
-        </KnappBase>
-    );
+const Knapp = ({ handleButtonClick, tekst }: LoggInnKnappProps) => (
+    <KnappBase
+        type="standard"
+        className="login-knapp"
+        id="desktop-login-knapp"
+        onClick={handleButtonClick}
+    >
+        {tekst}
+    </KnappBase>
+);
 
-    return <LoggInn Knapp={knapp} />;
-};
+export const LoggInnKnappDesktop = () => <LoggInn Knapp={Knapp} />;
--- src/komponenter/header/header-regular/meny/logginn/LoggInnKnappMobil.tsx
+++ src/komponenter/header/header-regular/meny/logginn/LoggInnKnappMobil.tsx
@@ -4,16 +4,14 @@ import { LoggInnKnappProps } from 'komponenter/header/header-regular/meny/loggin
 import LoggInn from 'komponenter/header/header-regular/meny/logginn/LoggInn';
 import Undertittel from 'nav-frontend-typografi/lib/undertittel';
 
-export const LoggInnKnappMobil = () => {
-    const knapp = ({ handleButtonClick, tekst }: LoggInnKnappProps) => (
-        <KnappBase
-            type="flat"
-            className="mobil-login-knapp"
-            onClick={handleButtonClick}
-        >
-            <Undertittel className="knappetekst">{tekst}</Undertittel>
-        </KnappBase>
-    );
+const Knapp = ({ handleButtonClick, tekst }: LoggInnKnappProps) => (
+    <KnappBase
+        type="flat"
+        className="mobil-login-knapp"
+        onClick={handleButtonClick}
+    >
+        <Undertittel className="knappetekst">{tekst}</Undertittel>
+    </KnappBase>
+);
 
-    return <LoggInn Knapp={knapp} />;
-};
+export const LoggInnKnappMobil = () => <LoggInn Knapp={Knapp} />;
