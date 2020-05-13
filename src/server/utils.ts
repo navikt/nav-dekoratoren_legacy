7f0cac5506e0dd64e11ffc94a3f5be742d63895f
Morten Jansrud <morten.jansrud@nav.no>
2020-05-13 09:14:57 +0200

    Fiks context-parameter

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -14,29 +14,38 @@ interface Props {
 
 // Client environment
 // Obs! Don't expose secrets
-export const clientEnv = ({ req, cookies }: Props): EnvironmentState => ({
-    XP_BASE_URL: process.env.XP_BASE_URL as string,
-    APP_BASE_URL: process.env.APP_BASE_URL as string,
-    API_VARSELINNBOKS_URL: process.env.API_VARSELINNBOKS_URL as string,
-    MINSIDE_ARBEIDSGIVER_URL: process.env.MINSIDE_ARBEIDSGIVER_URL as string,
-    DITT_NAV_URL: process.env.DITT_NAV_URL as string,
-    LOGIN_URL: process.env.LOGIN_URL as string,
-    LOGOUT_URL: process.env.LOGOUT_URL as string,
-    ...(req.query && {
-        PARAMS: {
-            LANGUAGE: (req.query.language || Language.NORSK) as Language,
-            CONTEXT: (req.query.context as MenuValue) || MenuValue.IKKEBESTEMT,
-            SIMPLE: !!req.query.simple,
-            SIMPLE_HEADER: !!req.query.header,
-            SIMPLE_FOOTER: !!req.query.footer,
-            REDIRECT_TO_APP: !!req.query.redirectToApp,
-            LEVEL: (req.query.level || 'Level3') as string,
-        },
-    }),
-    ...(cookies && {
-        COOKIES: {
-            CONTEXT: cookies['decorator-context'] as MenuValue,
-            LANGUAGE: cookies['decorator-language'] as Language,
-        },
-    }),
-});
+export const clientEnv = ({ req, cookies }: Props): EnvironmentState => {
+    const chosenLanguage = (req.query.language?.toString().toUpperCase() ||
+        Language.NORSK) as Language;
+
+    const chosenContext = (req.query.context?.toString().toUpperCase() ||
+        MenuValue.IKKEBESTEMT) as MenuValue;
+
+    return {
+        XP_BASE_URL: process.env.XP_BASE_URL as string,
+        APP_BASE_URL: process.env.APP_BASE_URL as string,
+        API_VARSELINNBOKS_URL: process.env.API_VARSELINNBOKS_URL as string,
+        MINSIDE_ARBEIDSGIVER_URL: process.env
+            .MINSIDE_ARBEIDSGIVER_URL as string,
+        DITT_NAV_URL: process.env.DITT_NAV_URL as string,
+        LOGIN_URL: process.env.LOGIN_URL as string,
+        LOGOUT_URL: process.env.LOGOUT_URL as string,
+        ...(req.query && {
+            PARAMS: {
+                LANGUAGE: chosenLanguage,
+                CONTEXT: chosenContext,
+                SIMPLE: !!req.query.simple,
+                SIMPLE_HEADER: !!req.query.header,
+                SIMPLE_FOOTER: !!req.query.footer,
+                REDIRECT_TO_APP: !!req.query.redirectToApp,
+                LEVEL: (req.query.level || 'Level3') as string,
+            },
+        }),
+        ...(cookies && {
+            COOKIES: {
+                CONTEXT: cookies['decorator-context'] as MenuValue,
+                LANGUAGE: cookies['decorator-language'] as Language,
+            },
+        }),
+    };
+};
