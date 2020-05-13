687b48bf90093c500d7387cd1ce0422052e9168f
anders <anders@nomerstad.com>
2020-05-13 09:18:19 +0200

    Forsøker å importere kun regex modulen fra core-js

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import 'isomorphic-fetch';
-import 'core-js';
+import 'core-js/stable/regexp';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
