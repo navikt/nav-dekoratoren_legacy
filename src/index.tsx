05427aa5385db417638ba9ed3b107a55d92422ac
Tobias McVey <tobias.mcvey@nav.no>
2020-05-14 13:49:15 +0200

    added amplitude to dekoratoren with pageviews

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,7 @@ import { createStore } from './store';
 import { erDev, verifyWindowObj } from './utils/Environment';
 import { fetchEnv } from './utils/Environment';
 import { initGA } from './utils/google-analytics';
+import { initAmplitude } from './utils/amplitude';
 import Footer from './komponenter/footer/Footer';
 import Header from './komponenter/header/Header';
 import * as es6promise from 'es6-promise';
@@ -34,6 +35,7 @@ if (erDev) {
 
 const run = () => {
     initGA();
+    initAmplitude();
     fetchEnv()
         .then((environment) => {
             const cookies = new Cookies();
