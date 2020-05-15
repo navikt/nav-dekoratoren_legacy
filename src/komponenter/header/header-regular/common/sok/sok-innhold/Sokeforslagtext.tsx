ed4111dc4c601b6ab2cfb46f97e53095ac97791a
anders <anders@nomerstad.com>
2020-05-15 12:50:38 +0200

    Sørger for at shortcode/html tag ikke vises dersom den er trunkert i forhåndsvisning på søk

--- src/komponenter/header/header-regular/common/sok/sok-innhold/Sokeforslagtext.tsx
+++ src/komponenter/header/header-regular/common/sok/sok-innhold/Sokeforslagtext.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Normaltekst } from 'nav-frontend-typografi';
 
 const Sokeforslagtext = ({ highlight }: { highlight?: string }) => {
+    const filteredHighlight = highlight && highlight
+        .replace(/<\/?[^>]+(>|$)/g, '') // Remove html
+        .replace(/\[.*?(\])/g, '') // Remove shortcodes
+        .replace(/(\[|<).*?(\(...\))/g, '(...)'); // Remove incomplete html/shortcodes;
+
     return (
         <div className="sok-resultat-listItem-text">
             <Normaltekst>
                 {
-                    (highlight || '')
-                        .replace(/<\/?[^>]+(>|$)/g, '') // Remove html
-                        .replace(/\[.*?\]/g, '') // Remove shortcodes
+                    (filteredHighlight || '')
                 }
             </Normaltekst>
         </div>
