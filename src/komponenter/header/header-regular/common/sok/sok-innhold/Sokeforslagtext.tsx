915b48551c9a65da4a2858e070b07cbfb2c785db
anders <anders@nomerstad.com>
2020-05-15 13:53:13 +0200

    Optional chaining!

--- src/komponenter/header/header-regular/common/sok/sok-innhold/Sokeforslagtext.tsx
+++ src/komponenter/header/header-regular/common/sok/sok-innhold/Sokeforslagtext.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { Normaltekst } from 'nav-frontend-typografi';
 
 const Sokeforslagtext = ({ highlight }: { highlight?: string }) => {
-    const highlightFiltered =
-        highlight &&
-        highlight
-            .replace(/<\/?[^>]+(>|$)/g, '') // Remove html
-            .replace(/\[.*?(\])/g, '') // Remove shortcodes
-            .replace(/(\[|<).*?(\(...\))/g, ''); // Remove incomplete html/shortcodes;
+    const highlightFiltered = highlight
+        ?.replace(/<\/?[^>]+(>|$)/g, '') // Remove html
+        .replace(/\[.*?(\])/g, '') // Remove shortcodes
+        .replace(/(\[|<).*?(\(...\))/g, ''); // Remove incomplete html/shortcodes;
 
     return (
         <div className="sok-resultat-listItem-text">
