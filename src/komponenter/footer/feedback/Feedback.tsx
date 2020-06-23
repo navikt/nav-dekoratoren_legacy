c5940909d98bbedbd8d0e47d69b35348692ed063
Shahitha Sothinathan <shahitha.sothinathan@Shahithas-MacBook-Pro.local>
2020-06-23 14:38:40 +0200

    Lagt til grunnleggende knapper

--- src/komponenter/footer/feedback/Feedback.tsx
+++ src/komponenter/footer/feedback/Feedback.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import { Knapp } from 'nav-frontend-knapper-style';
+import React, { Fragment } from 'react';
+import { Knapp } from 'nav-frontend-knapper';
+import { Element } from 'nav-frontend-typografi';
+
 import './Feedback.less';
 
 const Feedback = () => {
     return (
-        
+       <div className="feedback-content">
+           <div className="tekst">
+               <Element> Fant du det du lette etter? </Element>
+           </div>
+           <div className="ja-knapp">
+               <Knapp> Ja </Knapp>
+           </div>
+           <div className="nei-knapp">
+               <Knapp> Nei </Knapp>
+           </div>
+       </div>
+
     );
 };
 
