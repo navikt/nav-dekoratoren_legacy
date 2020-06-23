cbd2fca2a11614523566b399b371110d6fc5822f
aa1202 <andreas.amundsen123@gmail.com>
2020-06-23 14:29:31 +0200

    La til fritekstboks for utypet tilbakemelding

--- src/komponenter/footer/feedback/Feedback.tsx
+++ src/komponenter/footer/feedback/Feedback.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Knapp } from 'nav-frontend-knapper-style';
 import './Feedback.less';
+import Elaborated from './feedback-elaborated/Elaborated';
 
 const Feedback = () => {
     return (
+        <Elaborated />
         
     );
 };
--- src/komponenter/footer/feedback/feedback-elaborated/Elaborated.tsx
+++ src/komponenter/footer/feedback/feedback-elaborated/Elaborated.tsx
@@ -1,14 +1,31 @@
 import React, { Fragment } from 'react';
 import { RadioGruppe, Radio } from 'nav-frontend-skjema';
+import { Textarea } from 'nav-frontend-skjema';
 
 const Elaborated = () => {
+
+    const textareaDidChange = () => {
+        console.log('Endret');
+        
+    }
+
     return (
         <Fragment>
             <RadioGruppe legend='Hva gikk galt?'>
-                <Radio label={'Siden kræsjet'} name="feil"/>
-                <Radio label={'Jeg fant ikke frem til inholdet'} name="feil"/>
+                <Radio label={'Jeg ble forvirret'} name='feil'/>
+                <Radio label={'For mye tekst'} name='feil'/>
+                <Radio label={'Annet. Gjerne spesifiser under'} name='feil' />
             </RadioGruppe>
+
+            <Textarea 
+                label='Din tilbakemelding'
+                inputMode='text'    
+                value=''
+                onChange={textareaDidChange}
+            >
+            </Textarea>
         </Fragment>
-        
     );
 };
+
+export default Elaborated;
