bf20a211d2ca3c35d22b89731d64906c24e3da67
Shahitha Sothinathan <shahitha.sothinathan@Shahithas-MacBook-Pro.local>
2020-06-23 12:34:41 +0200

    Initial commit

---
+++ src/komponenter/footer/feedback/Feedback.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Knapp } from 'nav-frontend-knapper-style';
+import './Feedback.less';
+
+const Feedback = () => {
+    return (
+        
+    );
+};
+
+export default Feedback;
--- src/komponenter/footer/footer-regular/FooterRegular.tsx
+++ src/komponenter/footer/footer-regular/FooterRegular.tsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import FooterTopp from './footer-topp/FooterTopp';
 import FooterBottom from './footer-bottom/FooterBottom';
+import Feedback from '../feedback/Feedback';
 
 const FooterRegular = () => {
     return (
         <Fragment>
+            <Feedback /> 
             <FooterTopp />
             <FooterBottom />
         </Fragment>
