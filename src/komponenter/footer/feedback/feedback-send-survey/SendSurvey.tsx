f646be274c03e35e8c9c21aa5f34c748488e33b4
aa1202 <andreas.amundsen123@gmail.com>
2020-06-24 10:17:38 +0200

    Format SendSurvey using Prettier

--- src/komponenter/footer/feedback/feedback-send-survey/SendSurvey.tsx
+++ src/komponenter/footer/feedback/feedback-send-survey/SendSurvey.tsx
@@ -1,41 +1,34 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import Tekstomrade from 'nav-frontend-tekstomrade';
 import { Input } from 'nav-frontend-skjema';
 import { Hovedknapp } from 'nav-frontend-knapper';
 
-import './SendSurvey.less'
-import e from 'express';
+import './SendSurvey.less';
 
 const SendSurvey = () => {
     const [email, setEmail] = useState({});
 
     const handleSubmit = () => {
-        console.log(`${email}`)
-    }
+        console.log(`${email}`);
+    };
 
     return (
-
-        <div className='send-survey-wrapper'>
+        <div className="send-survey-wrapper">
             <Tekstomrade>
                 {`
-                Din tilbakemelding er verdifull for oss\n\nDersom du har tid ønsker vi at du svarer på en undersøkelse om hva vi kan forbedre på våre sider. Undersøkelsen er anonym og tar kun noen minutter
+                Takk for din tilbakemelding\n\nDersom du har tid ønsker vi at du svarer på en undersøkelse om hva vi kan forbedre på våre sider. Undersøkelsen er anonym og tar kun noen minutter
                 `}
             </Tekstomrade>
 
             <Input
-                label='Din e-postaddresse'
-                name='email-input'
-                onChange={e => setEmail(e.target.value)}
-                />
+                label="Din e-postaddresse"
+                onChange={(e) => setEmail(e.target.value)}
+            />
 
-            <Hovedknapp
-                inputMode='text'
-                onClick={handleSubmit}
-            >
+            <Hovedknapp inputMode="text" onClick={handleSubmit}>
                 Videre
             </Hovedknapp>
-
         </div>
     );
 };
