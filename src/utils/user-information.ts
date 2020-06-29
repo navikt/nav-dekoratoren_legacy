0f2f9a13abde41490d340de40c0cb61953b70596
aa1202 <andreas.amundsen123@gmail.com>
2020-06-29 14:32:22 +0200

    Sørg for at kleintinformasjon samles og lagres korrekt

--- src/utils/user-information.ts
+++ src/utils/user-information.ts
@@ -1,31 +1,22 @@
 /*
 TODO Avgjøre om det trengs å ta personvernrelaterte hensyn her (ref lokasjon f.eks.)
 */
-var visitorInformation: {
-    coockiesEnabled = boolean,
-    browserLanguage: string,
+interface UserInformation {
+    coockiesEnabled: boolean,
     language: string,
     platform: string,
-    connectionSpeed: string,
     userAgent: string,
     webDriver: boolean,
 }
 
-const dictionary: { [parameter: string]: string } = {
-    a: 'foo',
-    b: 'bar',
-}
-
 export default function gatherInformation(userNavigator: Navigator) {
-    visitorInformation.coockiesEnabled = userNavigator.cookieEnabled;
-    //visitorInformation.browserLanguage = userNavigator.browserLanguage;
-    visitorInformation.language = userNavigator.language;
-    visitorInformation.platform = userNavigator.platform;
-    //visitorInformation.connectionSpeed = userNavigator.connectionSpeed;
-    visitorInformation.userAgent = userNavigator.userAgent;
-    visitorInformation.webDriver = userNavigator.webdriver;
-    //visitorInformation.geolocation = userNavigator.geolocation;
+    let userInformation: UserInformation = {
+        coockiesEnabled: userNavigator.cookieEnabled,
+        language: userNavigator.language,
+        platform: userNavigator.platform,
+        userAgent: userNavigator.userAgent,
+        webDriver: userNavigator.webdriver,
+    }
 
-
-    return visitorInformation
+    return userInformation
 }
