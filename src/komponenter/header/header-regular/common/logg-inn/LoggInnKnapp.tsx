e217815326abbe118da3219e95b6e5617ed560c8
anders <anders@nomerstad.com>
2020-06-29 15:47:53 +0200

    Viser login knapp når innlogget sesjon er utløpet

--- src/komponenter/header/header-regular/common/logg-inn/LoggInnKnapp.tsx
+++ src/komponenter/header/header-regular/common/logg-inn/LoggInnKnapp.tsx
@@ -7,20 +7,28 @@ import { AppState } from 'store/reducers';
 import { MenuValue } from 'utils/meny-storage-utils';
 import KnappBase from 'nav-frontend-knapper';
 import './LoggInnKnapp.less';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export const loginKnappId = 'login-knapp-id';
 
 const stateSelector = (state: AppState) => ({
     authenticated: state.innloggingsstatus.data.authenticated,
+    expireTime: state.innloggingsstatus.data.expireTime,
     arbeidsflate: state.arbeidsflate.status,
     language: state.language.language,
     environment: state.environment,
 });
 
 export const LoggInnKnapp = () => {
-    const { authenticated, arbeidsflate, language, environment } = useSelector(
-        stateSelector
-    );
+    const {
+        authenticated,
+        expireTime,
+        arbeidsflate,
+        language,
+        environment,
+    } = useSelector(stateSelector);
+    const [isExpired, setIsExpired] = useState(false);
 
     const handleButtonClick = () => {
         const { LOGIN_URL, DITT_NAV_URL, LOGOUT_URL } = environment;
@@ -40,15 +48,23 @@ export const LoggInnKnapp = () => {
             action: authenticated ? 'logg-ut' : 'logg-inn',
         });
 
-        return authenticated
+        return authenticated && !isExpired
             ? (window.location.href = LOGOUT_URL)
             : (window.location.href = loginUrl);
     };
 
     const knappetekst = finnTekst(
-        authenticated ? 'logg-ut-knapp' : 'logg-inn-knapp',
+        authenticated && !isExpired ? 'logg-ut-knapp' : 'logg-inn-knapp',
         language
     );
+
+    useEffect(() => {
+        const expireTestVar = expireTime || Math.floor(Date.now() / 1000 + 120);
+        const timeLeft = expireTestVar - Date.now() / 1000;
+        if (timeLeft > 0) {
+            setTimeout(() => setIsExpired(true), timeLeft * 1000);
+        }
+    }, [expireTime]);
 
     return (
         <div className={'login-knapp-container'}>
