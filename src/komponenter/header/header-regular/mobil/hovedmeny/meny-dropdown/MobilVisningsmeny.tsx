43d51d6d0b840cc850409fb80fd352efd6200163
Morten Jansrud <morten.jansrud@nav.no>
2020-06-30 10:56:56 +0200

    Refaktor av MobilVisningsmeny.tsx

--- src/komponenter/header/header-regular/mobil/hovedmeny/meny-dropdown/MobilVisningsmeny.tsx
+++ src/komponenter/header/header-regular/mobil/hovedmeny/meny-dropdown/MobilVisningsmeny.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HoyreChevron from 'nav-frontend-chevron/lib/hoyre-chevron';
 import BEMHelper from 'utils/bem';
 import { MenyNode } from 'store/reducers/menu-duck';
@@ -9,22 +9,14 @@ import Undermeny from './mobil-innhold/Undermeny';
 import Listelement from './mobil-innhold/Listelement';
 import MobilarbeidsflateValg from '../../arbeidsflatemeny/MobilarbeidsflateValg';
 import { AppState } from 'store/reducers';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import InnloggetBruker from './mobil-innhold/innloggetbruker/InnloggetBruker';
 import ForsideLenke from './mobil-innhold/ForsideLenke';
 import Dittnavmeny from './mobil-innhold/dittnavmeny/Dittnavmeny';
-import { InnloggingsstatusState } from 'store/reducers/innloggingsstatus-duck';
 import Sok from 'komponenter/header/header-regular/common/sok/Sok';
 import './MobilVisningsmeny.less';
 
-export const mobilSokInputId = `mobil-sok-input`;
-
-interface StateProps {
-    arbeidsflate: MenuValue;
-    innloggingsstatus: InnloggingsstatusState;
-}
-
-interface VisningsmenyProps {
+interface Props {
     classname: string;
     menyLenker: MenyNode;
     minsideLenker: MenyNode;
@@ -36,124 +28,93 @@ interface VisningsmenyProps {
     lang: Language;
 }
 
-interface State {
-    className: string;
-    lenker: MenyNode;
-}
-
-type Props = VisningsmenyProps & StateProps;
-
-class MobilVisningsmeny extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-
-        this.state = {
-            className: '',
-            lenker: this.props.menyLenker.children[0],
-        };
-    }
-
-    setMenyliste = (
-        event: React.MouseEvent<HTMLAnchorElement>,
-        meny: MenyNode
-    ) => {
-        event.preventDefault();
-        this.props.togglemenu();
-        this.setState({ lenker: meny });
-    };
-
-    render(): React.ReactNode {
-        const {
-            classname,
-            menyLenker,
-            menuIsOpen,
-            arbeidsflate,
-            lang,
-            underMenuIsOpen,
-            minsideLenker,
-        } = this.props;
-        const menyClass = BEMHelper(classname);
-        return (
-            <div className={menyClass.className}>
-                <section
-                    className={menyClass.element(
-                        'startmeny',
-                        menuIsOpen && !underMenuIsOpen ? 'active' : ''
-                    )}
-                >
-                    <Sok
-                        isOpen={menuIsOpen}
-                        dropdownTransitionMs={400}
-                        id={mobilSokInputId}
-                    />
-                    <InnloggetBruker />
-
-                    <ForsideLenke
-                        arbeidsflate={arbeidsflate}
-                        erInnlogget={
-                            this.props.innloggingsstatus.data.authenticated
-                        }
-                    />
-                    {this.props.innloggingsstatus.data.authenticated &&
-                        arbeidsflate === MenuValue.PRIVATPERSON && (
-                            <div
-                                className={menyClass.element('submeny', 'wrap')}
-                            >
-                                <Dittnavmeny
-                                    minsideLenker={minsideLenker}
-                                    className={menyClass.className}
-                                    openMeny={this.setMenyliste}
-                                />
-                            </div>
-                        )}
-                    <MenyIngress
-                        className={menyClass.element('meny', 'ingress')}
-                        inputext={arbeidsflate}
-                    />
-                    <ul className={menyClass.element('meny', 'mainlist')}>
-                        {menyLenker.children.map(
-                            (menyElement: MenyNode, index: number) => {
-                                return (
-                                    <Listelement
-                                        className={menyClass.className}
-                                        classElement="text-element"
-                                        key={index}
-                                    >
-                                        <a
-                                            className="lenke"
-                                            href="https://nav.no"
-                                            onClick={(event) =>
-                                                this.setMenyliste(
-                                                    event,
-                                                    menyElement
-                                                )
-                                            }
-                                        >
-                                            {menyElement.displayName}
-                                            <HoyreChevron />
-                                        </a>
-                                    </Listelement>
-                                );
-                            }
-                        )}
-                    </ul>
-                    {lang === Language.NORSK && (
-                        <MobilarbeidsflateValg lang={lang} />
-                    )}
-                </section>
-                <Undermeny
-                    className={menyClass.className}
-                    undermenyIsOpen={underMenuIsOpen}
-                    lenker={this.state.lenker}
-                />
-            </div>
-        );
-    }
-}
-
-const mapStateToProps = (state: AppState): StateProps => ({
+export const mobilSokInputId = `mobil-sok-input`;
+const stateSelector = (state: AppState) => ({
     arbeidsflate: state.arbeidsflate.status,
     innloggingsstatus: state.innloggingsstatus,
 });
 
-export default connect(mapStateToProps)(MobilVisningsmeny);
+const MobilVisningsmeny = (props: Props) => {
+    const { arbeidsflate, innloggingsstatus } = useSelector(stateSelector);
+    const [lenker, settLenker] = useState(props.menyLenker.children[0]);
+    const { lang, underMenuIsOpen, minsideLenker } = props;
+    const { classname, menyLenker, menuIsOpen } = props;
+    const menyClass = BEMHelper(classname);
+
+    const setMenyliste = (
+        event: React.MouseEvent<HTMLAnchorElement>,
+        meny: MenyNode
+    ) => {
+        event.preventDefault();
+        props.togglemenu();
+        settLenker(meny);
+    };
+
+    return (
+        <div className={menyClass.className}>
+            <section
+                className={menyClass.element(
+                    'startmeny',
+                    menuIsOpen && !underMenuIsOpen ? 'active' : ''
+                )}
+            >
+                <Sok
+                    isOpen={menuIsOpen}
+                    dropdownTransitionMs={400}
+                    id={mobilSokInputId}
+                />
+                <InnloggetBruker />
+                <ForsideLenke
+                    arbeidsflate={arbeidsflate}
+                    erInnlogget={innloggingsstatus.data.authenticated}
+                />
+                {innloggingsstatus.data.authenticated &&
+                    arbeidsflate === MenuValue.PRIVATPERSON && (
+                        <div className={menyClass.element('submeny', 'wrap')}>
+                            <Dittnavmeny
+                                minsideLenker={minsideLenker}
+                                className={menyClass.className}
+                                openMeny={setMenyliste}
+                            />
+                        </div>
+                    )}
+                <MenyIngress
+                    className={menyClass.element('meny', 'ingress')}
+                    inputext={arbeidsflate}
+                />
+                <ul className={menyClass.element('meny', 'mainlist')}>
+                    {menyLenker.children.map(
+                        (menyElement: MenyNode, index: number) => (
+                            <Listelement
+                                key={index}
+                                className={menyClass.className}
+                                classElement="text-element"
+                            >
+                                <a
+                                    className="lenke"
+                                    href="https://nav.no"
+                                    onClick={(e) =>
+                                        setMenyliste(e, menyElement)
+                                    }
+                                >
+                                    {menyElement.displayName}
+                                    <HoyreChevron />
+                                </a>
+                            </Listelement>
+                        )
+                    )}
+                </ul>
+                {lang === Language.NORSK && (
+                    <MobilarbeidsflateValg lang={lang} />
+                )}
+            </section>
+            <Undermeny
+                className={menyClass.className}
+                undermenyIsOpen={underMenuIsOpen}
+                lenker={lenker}
+            />
+        </div>
+    );
+};
+
+export default MobilVisningsmeny;
