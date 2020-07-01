052b56f224aba9fc824736a11f37379f1537cb4f
aa1202 <andreas.amundsen123@gmail.com>
2020-07-01 15:38:17 +0200

    Formater amplitude.ts

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -1,14 +1,16 @@
-import amplitude from 'amplitude-js';
 import { contentEngaged } from './content-engaged';
+import amplitude from "amplitude-js";
 
 export const initAmplitude = () => {
-    amplitude.getInstance().init('default', '', {
-        apiEndpoint: 'amplitude.nav.no/collect-auto',
-        saveEvents: false,
-        includeUtm: true,
-        includeReferrer: true,
-        platform: window.location.toString(),
-    });
+    if(amplitude){
+        amplitude.getInstance().init('default', '', {
+            apiEndpoint: 'amplitude.nav.no/collect-auto',
+            saveEvents: false,
+            includeUtm: true,
+            includeReferrer: true,
+            platform: window.location.toString(),
+        });
+    } 
     contentEngaged(1, () => {
         logAmplitudeEvent('sidevisning');
     });
@@ -19,6 +21,9 @@ export function logAmplitudeEvent(eventName: string, data?: any): Promise<any> {
         const eventData = data || {};
         eventData.origin = 'dekoratøren';
         eventData.originVersion = 'unknown';
-        amplitude.getInstance().logEvent(eventName, eventData, resolve);
+
+        if(amplitude){
+            amplitude.getInstance().logEvent(eventName, eventData, resolve);
+        }
     });
 }
