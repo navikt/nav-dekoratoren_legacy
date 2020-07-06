3f652f335a6ccc0429c92d5bdf535532270980cc
aa1202 <andreas.amundsen123@gmail.com>
2020-07-06 15:56:52 +0200

    Legg til returntype

--- src/utils/url-filter/url-filter.ts
+++ src/utils/url-filter/url-filter.ts
@@ -1,6 +1,6 @@
 let urlWhitelist = require('./url-whitelist.json')
 
-function checkIfContainsUrl(clientUrl: string) {
+function checkIfContainsUrl(clientUrl: string): boolean {
     return urlWhitelist["allowed_urls"].includes(clientUrl)
 
 }
