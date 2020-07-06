4b346a0ab5a26dbe5da3afcd4ce218479cee1b12
aa1202 <andreas.amundsen123@gmail.com>
2020-07-06 12:26:13 +0200

    Legg til filer som ble borte under merge

---
+++ src/komponenter/footer/feedback/feedback-elaborated/Elaborated.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import { Textarea, RadioPanelGruppe } from 'nav-frontend-skjema';
+import './Elaborated.less';
+import { Innholdstittel, Undertittel } from 'nav-frontend-typografi';
+import Alertstripe from 'nav-frontend-alertstriper';
+import { Hovedknapp } from 'nav-frontend-knapper';
+import gatherUserInformation from 'utils/user-information';
+import Tekst from 'tekster/finn-tekst';
+
+const Elaborated = () => {
+    const [errorTitle, setErrorTitle] = useState(String);
+    const [errorMessage, setErrorMessage] = useState(String);
+    const [radiobuttonErrorMessage, setRadiobuttonErrorMessage] = useState(String);
+
+    const submitFeedback = (evt: any) => {
+        evt.preventDefault();
+
+        if (!errorTitle.length) {
+            setRadiobuttonErrorMessage('Du må velge et alternativ');
+        } else {
+            setRadiobuttonErrorMessage('');
+
+            const report = {
+                errorTitle: errorTitle,
+                errorMessage: errorMessage,
+                clientInformation: gatherUserInformation(navigator),
+            };
+
+            console.log(report);
+        }
+    };
+
+    return (
+        <div className="elaborated-wrapper">
+            <Innholdstittel>
+                <Tekst id="rapporter-om-feil-mangler" />
+            </Innholdstittel>
+
+            <form onSubmit={submitFeedback}>
+                <RadioPanelGruppe
+                    name="errorTitle"
+                    legend="Type feil eller mangel"
+                    feil={radiobuttonErrorMessage}
+                    radios={[
+                        {
+                            label: 'Informasjon',
+                            value: 'informasjon',
+                            id: 'informasjon',
+                        },
+                        {
+                            label: 'Ytelse',
+                            value: 'ytelse',
+                            id: 'ytelse',
+                        },
+                        {
+                            label: 'Utseende',
+                            value: 'utseende',
+                            id: 'utseende',
+                        },
+                        {
+                            label: 'Bug',
+                            value: 'bug',
+                            id: 'bug',
+                        },
+                        {
+                            label: 'Annet',
+                            value: 'annet',
+                            id: 'annet',
+                        },
+                    ]}
+                    onChange={(e) => setErrorTitle(e.target.value)}
+                    checked={errorTitle}
+                />
+
+                <Undertittel>
+                    <Tekst id="din-tilbakemelding" />
+                </Undertittel>
+
+                <Alertstripe type="advarsel">
+                    <Tekst id="advarsel-om-personopplysninger" />
+                </Alertstripe>
+
+                <Textarea
+                    value={errorMessage}
+                    onChange={(e) => setErrorMessage(e.target.value)}
+                />
+
+                <Hovedknapp htmlType="submit">
+                    <Tekst id="send-inn-feilrapport" />
+                </Hovedknapp>
+            </form>
+        </div>
+    );
+};
+
+export default Elaborated;
