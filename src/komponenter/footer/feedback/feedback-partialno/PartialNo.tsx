c90ae23080c63d76a7b323af84530c39232baee7
aa1202 <andreas.amundsen123@gmail.com>
2020-07-13 13:16:22 +0200

    Fiks bug med registrering av valg med checkboxer

--- src/komponenter/footer/feedback/feedback-partialno/PartialNo.tsx
+++ src/komponenter/footer/feedback/feedback-partialno/PartialNo.tsx
@@ -6,25 +6,31 @@ import { Knapp, Hovedknapp } from 'nav-frontend-knapper';
 import Tekst from 'tekster/finn-tekst';
 import { CheckboxGruppe, Checkbox } from 'nav-frontend-skjema';
 import { verifyWindowObj } from 'utils/Environment';
+import Alertstripe from 'nav-frontend-alertstriper';
 
-const {logAmplitudeEvent } = verifyWindowObj()
+const { logAmplitudeEvent } = verifyWindowObj()
     ? require('utils/amplitude')
     : () => null;
 
-
 const PartialNo = (props: any) => {
-    const [aarsaker, setAarsaker] = useState<string[]>([]);
+    const [aarsak, setAarsak] = useState<string[]>([]);
     const [negMessage, setNegMessage] = useState(String);
+    let aarsaker = [...aarsak];
 
-    useEffect(() => {
-        setAarsaker(props.aarsaker);
-    }, [props]);
+    const onClickAarsak = (evt: any) => {
+        aarsaker.includes(evt.target.value)
+            ? (aarsaker = aarsaker.filter((e) => e !== evt.target.value))
+            : aarsaker.push(evt.target.value);
+
+        setAarsak(aarsaker);
+    };
 
     const submitFeedback = (evt: any) => {
         evt.preventDefault();
 
         const report = {
             negMessage: negMessage,
+            aarsaker: aarsaker,
         };
 
         console.log(report);
@@ -36,12 +42,12 @@ const PartialNo = (props: any) => {
                 <Tekst id="send-undersokelse-takk" />
             </Ingress>
 
-            <form
-                onSubmit={submitFeedback}
-                className="content"
-                onChange={(e) => setAarsaker(e.target.value)}
-            >
-                <CheckboxGruppe legend="Gi din vurdering av informasjonen på siden">
+            <form onSubmit={submitFeedback} className="content">
+                <CheckboxGruppe
+                    // @ts-ignore
+                    onChange={(e) => onClickAarsak(e)}
+                    legend="Gi din vurdering av informasjonen på siden"
+                >
                     <Checkbox label={'Lite oversiktlig'} value="neg1" />
                     <Checkbox label={'Lite forståelig'} value="neg2" />
                     <Checkbox
