de89356c04b7efee4b4f99b0445dd0be7ede33f4
Shahitha Sothinathan <shahitha.sothinathan@Shahithas-MacBook-Pro.local>
2020-07-14 12:37:17 +0200

    Implementerte amplitude i send-feedback

--- src/komponenter/footer/feedback/feedback-partialno/send-feedback.ts
+++ src/komponenter/footer/feedback/feedback-partialno/send-feedback.ts
@@ -1,30 +1,26 @@
-function sendFeedback(title: string[], message: string): void {
+import { verifyWindowObj } from 'utils/Environment';
+
+function sendFeedback(titles: string[], message: string):void {
     const report = {
-        feedbackTitle: title,
+        feedbackTitle: titles,
         feedbackMessage: message,
-    };
+    }
 
     // Log to Amplitude
-    /*  import { verifyWindowObj } from 'utils/Environment';
-    import amplitude from 'amplitude-js';
     const { logAmplitudeEvent } = verifyWindowObj() 
         ? require('utils/amplitude')
         : () => null;
         
-        amplitude.getInstance().logEvent('tilbakemelding', 
-        {svar: 'Lite oversiktlig', svar2: 'Lite forståelig',
-        svar3: 'Lite relevant info', svar4: 'Villedende'});
+    console.log(titles)
 
-        console.log(...title);
-
-    let splitteTitle(...title)) {
-        console.log(svar1);      
+    for (let title of titles) {
+        logAmplitudeEvent('tilbakemelding', {svar: title})
+        console.log(title)
     }
 
-    splitteTitle(...title); , 
-        { svar1:  }*/
 
     // Save to DB
-}
+    console.log(report);
+};
 
 export default sendFeedback;
