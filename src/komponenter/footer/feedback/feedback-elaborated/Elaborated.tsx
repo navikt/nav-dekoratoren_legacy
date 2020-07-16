570fe86b16c248070ff8a2107ef0e9d4e849a0a5
aa1202 <andreas.amundsen123@gmail.com>
2020-07-16 15:28:22 +0200

    Vis Takk komponent når feedback sendes inn

--- src/komponenter/footer/feedback/feedback-elaborated/Elaborated.tsx
+++ src/komponenter/footer/feedback/feedback-elaborated/Elaborated.tsx
@@ -9,6 +9,7 @@ import FeedbackMessage from '../common/FeedbackMessage';
 import sendFeedbackReport from './send-feedback-report';
 import { useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
+import Thankyou from '../feedback-thank-you/ThankYou';
 
 const { logAmplitudeEvent } = verifyWindowObj()
     ? require('utils/amplitude')
@@ -22,9 +23,13 @@ const Elaborated = () => {
         String
     );
 
+    const [thankYouMessage, setThankYouMessage] = useState(false);
+
     const { language } = useSelector((state: AppState) => state.language);
 
     const submitFeedback = (evt: any) => {
+        console.log(thankYouMessage)
+
         evt.preventDefault();
         logAmplitudeEvent('tilbakemelding_mangler', { svar: category });
 
@@ -37,59 +42,77 @@ const Elaborated = () => {
                 feedbackMessage,
                 language.toLowerCase()
             );
+            setThankYouMessage(true);
         }
     };
 
     return (
-        <div className="eloborated-container">
-            <Ingress>
-                <Tekst id="rapporter-om-feil-mangler" />
-            </Ingress>
+        <Fragment>
+            {!thankYouMessage ? (
+                <div className="eloborated-container">
+                    <Ingress>
+                        <Tekst id="rapporter-om-feil-mangler" />
+                    </Ingress>
 
-            <form onSubmit={submitFeedback} className="content">
-                <Element className="tekst"> Type feil eller mangel </Element>
+                    <form onSubmit={submitFeedback} className="content">
+                        <Element className="tekst">
+                            {' '}
+                            Type feil eller mangel{' '}
+                        </Element>
 
-                <RadioGruppe
-                    feil={radiobuttonErrorMessage}
-                    // @ts-ignore
-                    onChange={(e) => setCategory(e.target.value)}
-                    checked={category}
-                >
-                    <Radio
-                        label={'Informasjon'}
-                        name="feil"
-                        value="informasjon"
-                    />
-                    <Radio label={'Ytelse'} name="feil" value="ytelse" />
-                    <Radio label={'Utseende'} name="feil" value="utseende" />
-                    <Radio label={'Bug'} name="feil" value="bug" />
-                    <Radio label={'Annet'} name="feil" value="annet" />
-                </RadioGruppe>
+                        <RadioGruppe
+                            feil={radiobuttonErrorMessage}
+                            // @ts-ignore
+                            onChange={(e) => setCategory(e.target.value)}
+                            checked={category}
+                        >
+                            <Radio
+                                label={'Informasjon'}
+                                name="feil"
+                                value="informasjon"
+                            />
+                            <Radio
+                                label={'Ytelse'}
+                                name="feil"
+                                value="ytelse"
+                            />
+                            <Radio
+                                label={'Utseende'}
+                                name="feil"
+                                value="utseende"
+                            />
+                            <Radio label={'Bug'} name="feil" value="bug" />
+                            <Radio label={'Annet'} name="feil" value="annet" />
+                        </RadioGruppe>
 
-                <div>
-                    <Element className="tekst">
-                        <Tekst id="din-tilbakemelding" />
-                    </Element>
+                        <div>
+                            <Element className="tekst">
+                                <Tekst id="din-tilbakemelding" />
+                            </Element>
 
-                    <div className="advarsel">
-                        <Alertstripe type="advarsel">
-                            <Tekst id="advarsel-om-personopplysninger" />
-                        </Alertstripe>
-                    </div>
+                            <div className="advarsel">
+                                <Alertstripe type="advarsel">
+                                    <Tekst id="advarsel-om-personopplysninger" />
+                                </Alertstripe>
+                            </div>
 
-                    <FeedbackMessage
-                        feedbackMessage={feedbackMessage}
-                        setFeedbackMessage={setFeedbackMessage}
-                    />
+                            <FeedbackMessage
+                                feedbackMessage={feedbackMessage}
+                                setFeedbackMessage={setFeedbackMessage}
+                            />
 
-                    <div className="submit-knapp">
-                        <Hovedknapp htmlType="submit">
-                            <Tekst id="send-inn-feilrapport" />
-                        </Hovedknapp>
-                    </div>
+                            <div className="submit-knapp">
+                                <Hovedknapp htmlType="submit">
+                                    <Tekst id="send-inn-feilrapport" />
+                                </Hovedknapp>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
+            ) : (
+                <Thankyou />
+            )}
+        </Fragment>
     );
 };
 
--- src/komponenter/footer/feedback/feedback-partialno/PartialNo.tsx
+++ src/komponenter/footer/feedback/feedback-partialno/PartialNo.tsx
@@ -10,6 +10,8 @@ import FeedbackMessage from '../common/FeedbackMessage';
 import sendFeedbackNo from './send-feedback-no';
 import { useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
+import { set } from 'react-ga';
+import Thankyou from '../feedback-thank-you/ThankYou';
 
 const { logAmplitudeEvent } = verifyWindowObj()
     ? require('utils/amplitude')
@@ -19,6 +21,8 @@ const PartialNo = (props: any) => {
     const [feedbackTitle, setFeedbackTitle] = useState<string[]>([]);
     const [feedbackMessage, setFeedbackMessage] = useState('');
     const { language } = useSelector((state: AppState) => state.language);
+
+    const [thankYouMessage, setThankYouMessage] = useState(false);
 
     let feedbackTitles = [...feedbackTitle];
 
@@ -36,54 +40,63 @@ const PartialNo = (props: any) => {
         evt.preventDefault();
 
         sendFeedbackNo(feedbackTitle, feedbackMessage, language.toLowerCase());
+        setThankYouMessage(true);
     };
 
     return (
-        <div className="partialno-container">
-            <Ingress>
-                <Tekst id="send-undersokelse-takk" />
-            </Ingress>
+        <div>
+            {!thankYouMessage ? (
+                <div className="partialno-container">
+                    <Ingress>
+                        <Tekst id="send-undersokelse-takk" />
+                    </Ingress>
 
-            <form onSubmit={submitFeedback} className="content">
-                <CheckboxGruppe
-                    // @ts-ignore
-                    onChange={(e) => onClickAarsak(e)}
-                    legend="Gi din vurdering av informasjonen på siden"
-                >
-                    <Checkbox
-                        label={'Lite oversiktlig'}
-                        value="Lite oversiktlig"
-                    />
-                    <Checkbox
-                        label={'Lite forståelig'}
-                        value="Lite forståelig"
-                    />
-                    <Checkbox
-                        label={'Lite relevant informasjon'}
-                        value="Lite relevant informasjon"
-                    />
-                    <Checkbox label={'Villedende'} value="Villedende" />
-                </CheckboxGruppe>
+                    <form onSubmit={submitFeedback} className="content">
+                        <CheckboxGruppe
+                            // @ts-ignore
+                            onChange={(e) => onClickAarsak(e)}
+                            legend="Gi din vurdering av informasjonen på siden"
+                        >
+                            <Checkbox
+                                label={'Lite oversiktlig'}
+                                value="Lite oversiktlig"
+                            />
+                            <Checkbox
+                                label={'Lite forståelig'}
+                                value="Lite forståelig"
+                            />
+                            <Checkbox
+                                label={'Lite relevant informasjon'}
+                                value="Lite relevant informasjon"
+                            />
+                            <Checkbox label={'Villedende'} value="Villedende" />
+                        </CheckboxGruppe>
 
-                <div className="content">
-                    <Element>Noe annet? Spesifiser gjerne nedenfor.</Element>
+                        <div className="content">
+                            <Element>
+                                Noe annet? Spesifiser gjerne nedenfor.
+                            </Element>
 
-                    <div className="advarsel">
-                        <Alertstripe type="advarsel">
-                            <Tekst id="advarsel-om-personopplysninger" />
-                        </Alertstripe>
-                    </div>
+                            <div className="advarsel">
+                                <Alertstripe type="advarsel">
+                                    <Tekst id="advarsel-om-personopplysninger" />
+                                </Alertstripe>
+                            </div>
 
-                    <FeedbackMessage
-                        feedbackMessage={feedbackMessage}
-                        setFeedbackMessage={setFeedbackMessage}
-                    />
+                            <FeedbackMessage
+                                feedbackMessage={feedbackMessage}
+                                setFeedbackMessage={setFeedbackMessage}
+                            />
 
-                    <div className="submit-knapp">
-                        <Hovedknapp htmlType="submit">Send inn</Hovedknapp>
-                    </div>
+                            <div className="submit-knapp">
+                                <Hovedknapp htmlType="submit">
+                                    Send inn
+                                </Hovedknapp>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            ) : <Thankyou />}
         </div>
     );
 };
