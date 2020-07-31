9309ea418830abde07cbde194c4c88ae4865ca65
aa1202 <andreas.amundsen123@gmail.com>
2020-07-31 12:50:24 +0200

    Refaktorer

--- src/utils/text-filter/Filter.ts
+++ src/utils/text-filter/Filter.ts
@@ -1,8 +1,6 @@
 import { EmailValidator } from './validators/EmailValidator';
 import { FodselsnummerValidator } from './validators/FodselsnummerValidator';
 
-const sampleText = 'Hei jeg heter Andreas Amundsen og min epostaddresse er andreas@gmail.com 12345678901'
-
 export class Filter {
 
     violations: string[]
@@ -16,23 +14,23 @@ export class Filter {
     }
 
     addViolation(violation: string) {
-        !this.violations.includes(violation) && this.violations.push(violation);
+        if (!this.violations.includes(violation)) {
+            this.violations.push(violation)
+        }
     }
 
     getViolations(): string[] {
         return this.violations;
     }
 
-    /*
-    Return violations on either the form "e-postaddresse og fødselsnummer" or "e-postaddresse"/"fødeselsnummer", depending on the numer
-    of violations
-    */
     getViolationsFormatted(): string {
         if (!this.violations.length) {
             return '';
         }
 
-        return this.violations.length === 2 ? '' + this.violations[0] + ' og ' + this.violations[1] : ' ' + this.violations[0]
+        return this.violations.length === 2 
+            ? '' + this.violations[0] + ' og ' + this.violations[1] 
+            : ' ' + this.violations[0]
     }
 
     checkForViolations(text: string): void {
