c333e4545058990b6e77c7f6db183f3d4eba9887
aa1202 <andreas.amundsen123@gmail.com>
2020-08-04 15:54:23 +0200

    Dokumenter Filter

--- src/utils/text-filter/Filter.ts
+++ src/utils/text-filter/Filter.ts
@@ -1,6 +1,21 @@
 import { EmailValidator } from './validators/EmailValidator';
 import { FodselsnummerValidator } from './validators/FodselsnummerValidator';
 
+/*
+
+Tar inn fritekst og finner ut om den inneholder enten en e-postadresse eller fødselsnummer
+
+Foreslåtte bruksområder:
+- Koble mot et onChange event på tekstfelt, slik at brukeren får varsel når enten e-postadresse eller fødselsnummer er skrevet inn
+- Koble opp mot 'Send inn'-knapp i Elaborated og ParitalNo
+
+Dersom brukeren skriver inn enten e-postaddresse eller fødselsnummer bør dette bli varslet om i feiloppsummeringsboksen
+
+Flere validatorer kan legges til ved å implementere TextValidator interface
+
+*/
+
+
 export class Filter {
 
     violations: string[]
@@ -13,6 +28,11 @@ export class Filter {
         this.fodselsnummerValidator = new FodselsnummerValidator();
     }
 
+    /*
+
+    Legg til en overtredelse hvis den ikke finnes fra før
+
+    */
     addViolation(violation: string) {
         if (!this.violations.includes(violation)) {
             this.violations.push(violation)
@@ -33,6 +53,12 @@ export class Filter {
             : ' ' + this.violations[0]
     }
 
+    /*
+
+    Splitter tekst på mellomrom og kjører sjekk (e-postaddresse/fødselsnummer) for hvert element i listen. 
+    Dersom overtredelser blir oppdaget blir dette lagt til i violations
+
+    */
     checkForViolations(text: string): void {
         const textSplitted = text.split(' ');
 
