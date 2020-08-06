e16cbb628f32ea65743dae3be17916866cb73058
anders <anders@nomerstad.com>
2020-08-06 17:16:08 +0200

    Fikser sticky-header når en klikker på mobilmenyen ved halvveis inn-scrollet menylinje

--- src/komponenter/header/header-regular/common/sticky/Sticky.tsx
+++ src/komponenter/header/header-regular/common/sticky/Sticky.tsx
@@ -6,10 +6,11 @@ import { getLinkAnchorId } from 'komponenter/header/header-regular/common/sticky
 import './Sticky.less';
 
 type Props = {
+    mobilFixed?: boolean;
     children: JSX.Element;
 };
 
-export const Sticky = ({ children }: Props) => {
+export const Sticky = ({ mobilFixed, children }: Props) => {
     const prevScrollOffset = useRef(0);
     const baseOffset = useRef(0);
 
@@ -75,7 +76,12 @@ export const Sticky = ({ children }: Props) => {
 
     return (
         <div className={'sticky-placeholder'} ref={placeholderRef}>
-            <div className={`sticky-container`} ref={stickyRef}>
+            <div
+                className={`sticky-container ${
+                    mobilFixed ? 'sticky-container--mobil-fixed' : ''
+                }`}
+                ref={stickyRef}
+            >
                 {children}
             </div>
         </div>
