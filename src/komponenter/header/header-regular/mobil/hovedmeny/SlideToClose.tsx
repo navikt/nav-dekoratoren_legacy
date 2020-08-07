847492ee944fd5e627b3921a35b4848e7ba89938
anders <anders@nomerstad.com>
2020-08-07 12:26:43 +0200

    Fork

--- src/komponenter/header/header-regular/mobil/hovedmeny/SlideToClose.tsx
+++ src/komponenter/header/header-regular/mobil/hovedmeny/SlideToClose.tsx
@@ -7,21 +7,43 @@ interface Props {
     className?: string;
 }
 
+const slideTriggerRate = 2;
+const slideTriggerDx = 25;
+
 export const SlideToClose = ({ children, className }: Props) => {
+    const [isSliding, setIsSliding] = useState(false);
+    const [disableSliding, setDisableSliding] = useState(false);
     const [startX, setStartX] = useState(0);
     const [startY, setStartY] = useState(0);
     const [dx, setDx] = useState(0);
-    const style = dx ? { left: -dx, transition: 'none' } : undefined;
+    const style = dx
+        ? {
+              left: -dx,
+              transition: 'none',
+          }
+        : undefined;
     const dispatch = useDispatch();
 
     const onTouchMove = (event: TouchEvent<HTMLElement>) => {
+        if (disableSliding) {
+            return;
+        }
+
         const dx = startX - event.touches[0].clientX;
-        const dy = startY - event.touches[0].clientY;
-        if (Math.abs(dy) > 20) {
-            g;
-            setDx(0);
-        } else if (dx > 0) {
-            setDx(dx);
+
+        if (isSliding) {
+            if (dx > 0) {
+                setDx(dx);
+            }
+        } else {
+            if (dx >= slideTriggerDx) {
+                const dy = startY - event.touches[0].clientY;
+                if (dx / Math.abs(dy) > slideTriggerRate) {
+                    setIsSliding(true);
+                } else {
+                    setDisableSliding(true);
+                }
+            }
         }
     };
 
@@ -32,6 +54,8 @@ export const SlideToClose = ({ children, className }: Props) => {
 
     const onTouchEnd = (event: TouchEvent<HTMLElement>) => {
         setDx(0);
+        setIsSliding(false);
+        setDisableSliding(false);
         if (dx > 50) {
             dispatch(toggleHovedmeny());
         }
@@ -43,6 +67,7 @@ export const SlideToClose = ({ children, className }: Props) => {
             onTouchStart={onTouchStart}
             onTouchMove={onTouchMove}
             onTouchEnd={onTouchEnd}
+            onScroll={() => setDisableSliding(true)}
             className={className}
             style={style}
         >
