613740652135c0c773048e5df54a5b47d99333e1
anders <anders@nomerstad.com>
2020-08-12 08:46:36 +0200

    Tilpasser lenkepanel for context for mobil, og tar i bruk disse

---
+++ src/komponenter/header/header-regular/common/context-lenkepanel/ContextLenkepanel.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Tekst from 'tekster/finn-tekst';
+import { Undertekst, Undertittel } from 'nav-frontend-typografi';
+import { GACategory } from 'utils/google-analytics';
+import { gaEvent } from 'utils/google-analytics';
+import BEMHelper from 'utils/bem';
+import { LenkepanelBase } from 'nav-frontend-lenkepanel/lib';
+import { HoyreChevron } from 'nav-frontend-chevron';
+import { useDispatch } from 'react-redux';
+import { ArbeidsflateLenke } from 'komponenter/common/arbeidsflate-lenker/arbeidsflate-lenker';
+import { Language } from 'store/reducers/language-duck';
+import { useCookies } from 'react-cookie';
+import { cookieOptions } from 'store/reducers/arbeidsflate-duck';
+import { erNavDekoratoren } from 'utils/Environment';
+import { settArbeidsflate } from 'store/reducers/arbeidsflate-duck';
+import { finnTekst } from 'tekster/finn-tekst';
+import './ContextLenkepanel.less';
+
+interface Props {
+    lenke: ArbeidsflateLenke;
+    language: Language;
+    id?: string;
+}
+
+const ContextLenkepanel = ({ lenke, language, id }: Props) => {
+    const cls = BEMHelper('meny-contextlenke');
+    const dispatch = useDispatch();
+    const [, setCookie] = useCookies();
+
+    return (
+        <LenkepanelBase
+            href={lenke.url}
+            className={cls.className}
+            id={id}
+            onClick={(event) => {
+                setCookie('decorator-context', lenke.key, cookieOptions);
+                if (erNavDekoratoren()) {
+                    event.preventDefault();
+                    dispatch(settArbeidsflate(lenke.key));
+                }
+                gaEvent({
+                    category: GACategory.Meny,
+                    action: `hovedmeny/arbeidsflatelenke`,
+                    label: lenke.url,
+                });
+            }}
+            border={true}
+        >
+            <div className={cls.element('innhold')}>
+                <Undertittel className={'lenkepanel__heading'}>
+                    <HoyreChevron className={cls.element('chevron')} />
+                    <Tekst id={lenke.lenkeTekstId} />
+                </Undertittel>
+                <Undertekst className={cls.element('stikkord')}>
+                    {finnTekst(lenke.stikkordId, language)}
+                </Undertekst>
+            </div>
+        </LenkepanelBase>
+    );
+};
+
+export default ContextLenkepanel;
--- src/komponenter/header/header-regular/mobil/meny/innhold/elementer/arbeidsflatemeny/MobilarbeidsflateValg.tsx
+++ src/komponenter/header/header-regular/mobil/meny/innhold/elementer/arbeidsflatemeny/MobilarbeidsflateValg.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
-import Undertittel from 'nav-frontend-typografi/lib/undertittel';
 import BEMHelper from 'utils/bem';
-import { settArbeidsflate } from 'store/reducers/arbeidsflate-duck';
-import { cookieOptions } from 'store/reducers/arbeidsflate-duck';
-import { GACategory } from 'utils/google-analytics';
-import { LenkeMedGA } from 'komponenter/common/lenke-med-ga/LenkeMedGA';
-import Normaltekst from 'nav-frontend-typografi/lib/normaltekst';
-import { finnTekst } from 'tekster/finn-tekst';
 import { Language } from 'store/reducers/language-duck';
-import { erNavDekoratoren } from 'utils/Environment';
-import { useCookies } from 'react-cookie';
-import Tekst from 'tekster/finn-tekst';
 import { bunnLenker } from 'komponenter/common/arbeidsflate-lenker/hovedmeny-arbeidsflate-lenker';
-import './MobilarbeidsflateValg.less';
-import BunnseksjonLenke from 'komponenter/header/header-regular/desktop/hovedmeny/bunn-seksjon/BunnseksjonLenke';
-import KbNav from 'utils/keyboard-navigation/kb-navigation';
-import { KbNavGroup } from 'utils/keyboard-navigation/kb-navigation';
+import ContextLenkepanel from 'komponenter/header/header-regular/common/context-lenkepanel/ContextLenkepanel';
 import { ArbeidsflateLenke } from 'komponenter/common/arbeidsflate-lenker/arbeidsflate-lenker';
+import './MobilarbeidsflateValg.less';
 
 interface Props {
     lang: Language;
@@ -30,37 +18,15 @@ const stateProps = (state: AppState) => ({
 });
 
 const MobilarbeidsflateValg = ({ lang }: Props) => {
-    const dispatch = useDispatch();
-    const [, setCookie] = useCookies(['decorator-context']);
     const { arbeidsflate, environment } = useSelector(stateProps);
     const cls = BEMHelper('mobil-arbeidsflate-valg');
-    const lenker = bunnLenker(environment)[arbeidsflate];
+    const lenker = bunnLenker(environment)[arbeidsflate] as ArbeidsflateLenke[];
 
     return (
         <ul className={cls.className}>
             {lenker.map((lenke, i) => (
                 <li key={i} className={cls.element('liste-element')}>
-                    <BunnseksjonLenke
-                        url={lenke.url}
-                        lenkeTekstId={lenke.lenkeTekstId}
-                        stikkord={finnTekst(lenke.stikkordId, lang)}
-                        className={cls.className}
-                        onClick={(event) => {
-                            event.preventDefault();
-                            const context = lenke as ArbeidsflateLenke;
-                            setCookie(
-                                'decorator-context',
-                                context.key,
-                                cookieOptions
-                            );
-                            if (erNavDekoratoren()) {
-                                dispatch(settArbeidsflate(context.key));
-                            } else {
-                                window.location.href = context.url;
-                            }
-                        }}
-                        key={lenke.lenkeTekstId}
-                    />
+                    <ContextLenkepanel lenke={lenke} language={lang} />
                 </li>
             ))}
         </ul>
