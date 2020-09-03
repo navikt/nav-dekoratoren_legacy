e98557cbcc315d106214247ecf0443d43e2391c4
Morten Jansrud <morten.jansrud@nav.no>
2020-09-03 09:33:46 +0200

    Legg til hjem-ikon

--- src/komponenter/header/common/brodsmulesti/Brodsmulesti.tsx
+++ src/komponenter/header/common/brodsmulesti/Brodsmulesti.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
-import './Brodsmulesti.less';
 import Lenke from 'nav-frontend-lenker';
-import BEMHelper from '../../../../utils/bem';
+import BEMHelper from 'utils/bem';
+import { Bilde } from '../../../common/bilde/Bilde';
+import HomeIcon from 'ikoner/home.svg';
+import './Brodsmulesti.less';
+import { Normaltekst } from 'nav-frontend-typografi';
+import { HoyreChevron } from 'nav-frontend-chevron';
 
 export interface Breadcrumb {
     url: string;
@@ -12,16 +16,25 @@ export interface Breadcrumb {
 
 export const Brodsmulesti = () => {
     const { environment } = useSelector((state: AppState) => state);
+    const { XP_BASE_URL } = environment;
     const { BREADCRUMBS } = environment.PARAMS;
     const cls = BEMHelper('brodsmulesti');
 
     return BREADCRUMBS ? (
         <div className={cls.element('container')}>
+            <Bilde asset={HomeIcon} />
+            <Lenke href={XP_BASE_URL}>
+                <Normaltekst>nav.no</Normaltekst>
+            </Lenke>
+            <HoyreChevron />
             {BREADCRUMBS.map((breadcrumb, i) => {
                 return (
-                    <Lenke key={i} href={breadcrumb.url}>
-                        {breadcrumb.name}
-                    </Lenke>
+                    <>
+                        <Lenke key={i} href={breadcrumb.url}>
+                            {breadcrumb.name}
+                        </Lenke>
+                        <HoyreChevron />
+                    </>
                 );
             })}
         </div>
