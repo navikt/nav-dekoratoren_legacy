3c100c7bbf67b8f708e05deefece1d23c827329b
Morten Jansrud <morten.jansrud@nav.no>
2020-09-14 12:15:40 +0200

    Send brukeren til forsiden til respektivt språk

--- src/komponenter/common/nav-logo/NavLogoLenke.tsx
+++ src/komponenter/common/nav-logo/NavLogoLenke.tsx
@@ -4,13 +4,16 @@ import { getArbeidsflateContext } from 'komponenter/common/arbeidsflate-lenker/a
 import { MenuValue } from 'utils/meny-storage-utils';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
-import { settArbeidsflate } from 'store/reducers/arbeidsflate-duck';
-import { cookieOptions } from 'store/reducers/arbeidsflate-duck';
+import {
+    cookieOptions,
+    settArbeidsflate,
+} from 'store/reducers/arbeidsflate-duck';
 import { erNavDekoratoren } from 'utils/Environment';
 import { useCookies } from 'react-cookie';
 import { AnalyticsEventArgs } from 'utils/analytics';
 import { Bilde } from 'komponenter/common/bilde/Bilde';
 import './NavLogoLenke.less';
+import { Locale } from '../../../store/reducers/language-duck';
 
 type Props = {
     analyticsEventArgs: AnalyticsEventArgs;
@@ -22,12 +25,20 @@ export const NavLogoLenke = (props: Props) => {
     const dispatch = useDispatch();
     const [, setCookie] = useCookies(['decorator-context']);
     const { XP_BASE_URL } = useSelector((state: AppState) => state.environment);
+    const { language } = useSelector((state: AppState) => state.language);
     const context = getArbeidsflateContext(XP_BASE_URL, MenuValue.PRIVATPERSON);
+    const urlMap: { [key: string]: string } = {
+        nb: context.url,
+        nn: context.url,
+        en: `${XP_BASE_URL}/en/home`,
+        se: `${XP_BASE_URL}/se/samegiella`,
+    };
 
+    const url = urlMap[language];
     return (
         <LenkeMedSporing
             classNameOverride={'nav-logo-lenke'}
-            href={context.url}
+            href={url}
             analyticsEventArgs={props.analyticsEventArgs}
             onClick={(event) => {
                 event.preventDefault();
@@ -35,7 +46,7 @@ export const NavLogoLenke = (props: Props) => {
                 if (erNavDekoratoren()) {
                     dispatch(settArbeidsflate(context.key));
                 } else {
-                    window.location.href = context.url;
+                    window.location.href = url;
                 }
             }}
             id={props.id}
