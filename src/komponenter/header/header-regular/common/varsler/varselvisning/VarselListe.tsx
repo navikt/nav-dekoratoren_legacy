1d3758719fa28d7ad1bc4b6a108443511bfcb9ee
Ida Onshus <Ida.Onshus@nav.no>
2020-10-14 14:19:47 +0200

    rydder opp i bruk av section tags

--- src/komponenter/header/header-regular/common/varsler/varselvisning/VarselListe.tsx
+++ src/komponenter/header/header-regular/common/varsler/varselvisning/VarselListe.tsx
@@ -14,7 +14,6 @@ import chatIkon from 'ikoner/varsler/bubble-chat-2.svg';
 import dokumentIkon from 'ikoner/varsler/file-new-1.svg';
 import plasterIkon from 'ikoner/varsler/first-aid-plaster.svg';
 
-
 const ikoner: { [str: string]: string } = {
     'alarm-ikon': alarmIkon,
     'kalender-ikon': kalenderIkon,
@@ -24,28 +23,28 @@ const ikoner: { [str: string]: string } = {
 };
 
 type Props = {
-    varsler: NyesteVarslerData[],
+    varsler: NyesteVarslerData[];
     rowIndex?: number;
 };
 
 interface VarselConfig {
     ikontekst: string;
-    varselType: string,
-    stylingklasse: string,
-    lenketekst: string
+    varselType: string;
+    stylingklasse: string;
+    lenketekst: string;
 }
 
 const defaultConfig = {
     ikontekst: 'alarm-ikon',
     varselType: '',
     stylingklasse: '',
-    lenketekst: 'Lenke'
-}
+    lenketekst: 'Lenke',
+};
 const getVarselTypeConfig = (varselType: string) => {
     const data: VarselConfig[] = varselConfig;
-    const config = data.find( (item: VarselConfig) => item.varselType === varselType );
+    const config = data.find((item: VarselConfig) => item.varselType === varselType);
     return config ? config : defaultConfig;
-}
+};
 
 const formatDato = (datoString: string) => {
     const datoObjekt = new Date(parseInt(datoString, 10));
@@ -53,67 +52,56 @@ const formatDato = (datoString: string) => {
         return '';
     }
 
-    const datoOptions = {day: '2-digit', month: 'long', year: 'numeric' };
-    const datoFormattert = new Intl.DateTimeFormat('nb', datoOptions).format(datoObjekt)
-    const klokkeslettOptions = {  hour: 'numeric', minute: 'numeric'};
-    const klokkeslettFormattert =  new Intl.DateTimeFormat('nb', klokkeslettOptions).format(datoObjekt)
-    return `${datoFormattert} kl ${klokkeslettFormattert}`
-}
+    const datoOptions = { day: '2-digit', month: 'long', year: 'numeric' };
+    const datoFormattert = new Intl.DateTimeFormat('nb', datoOptions).format(datoObjekt);
+    const klokkeslettOptions = { hour: 'numeric', minute: 'numeric' };
+    const klokkeslettFormattert = new Intl.DateTimeFormat('nb', klokkeslettOptions).format(datoObjekt);
+    return `${datoFormattert} kl ${klokkeslettFormattert}`;
+};
 
 export const VarselListe = ({ varsler, rowIndex }: Props) => {
-
-    const arbeidsflate = useSelector(
-        (state: AppState) => state.arbeidsflate.status
-    );
+    const arbeidsflate = useSelector((state: AppState) => state.arbeidsflate.status);
 
     return (
         <ul>
-            {
-                varsler.map( (varsel: NyesteVarslerData, subIndex) => {
-                    const currentConfig = getVarselTypeConfig((varsel.meldingsType));
-                    const ikon = ikoner[currentConfig.ikontekst] || alarmIkon;
+            {varsler.map((varsel: NyesteVarslerData, subIndex) => {
+                const currentConfig = getVarselTypeConfig(varsel.meldingsType);
+                const ikon = ikoner[currentConfig.ikontekst] || alarmIkon;
 
-                    return (
-                        <li key={varsel.varselId} className="dekorator-varsel-container">
-                            <section className="dekorator-varsel">
-                                <div className="varsel-ikon-row">
-                                    <div className={`varsel-ikon-container ${currentConfig.ikontekst}`}>
-                                        <Bilde
-                                            asset={ikon}
-                                            altText={'varsel-ikon'}
-                                            className={`varsel-ikon`}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="varsel-innhold">
-                                    <p className="typo-normal-liten varsel-dato">{formatDato(varsel.datoOpprettet)}</p>
-                                    <p className="typo-infotekst">{varsel.varseltekst}</p>
-                                    <LenkeMedSporing
-                                        href={varsel.url || ''}
-                                        id={
-                                            rowIndex !== undefined && subIndex !== undefined
-                                                ? getKbId(KbNavGroup.Varsler, {
-                                                    col: 0,
-                                                    row: rowIndex,
-                                                    sub: subIndex,
-                                                })
-                                                : undefined
-                                        }
-                                        analyticsEventArgs={{
-                                            context: arbeidsflate,
-                                            category: AnalyticsCategory.Header,
-                                            action: 'varsel-lenke',
-                                            label: varsel.url,
-                                        }}
-                                    >
-                                        {currentConfig.lenketekst}
-                                    </LenkeMedSporing>
-                                </div>
-                            </section>
-                        </li>
-                    );
-                })
-            }
+                return (
+                    <li key={varsel.varselId} className="dekorator-varsel-container">
+                        <div className="dekorator-varsel">
+                            <div className={`varsel-ikon-container ${currentConfig.ikontekst}`}>
+                                <Bilde asset={ikon} altText={'varsel-ikon'} className={`varsel-ikon`} />
+                            </div>
+                            <div className="varsel-innhold">
+                                <p className="typo-normal-liten varsel-dato">{formatDato(varsel.datoOpprettet)}</p>
+                                <p className="typo-infotekst">{varsel.varseltekst}</p>
+                                <LenkeMedSporing
+                                    href={varsel.url || ''}
+                                    id={
+                                        rowIndex !== undefined && subIndex !== undefined
+                                            ? getKbId(KbNavGroup.Varsler, {
+                                                  col: 0,
+                                                  row: rowIndex,
+                                                  sub: subIndex,
+                                              })
+                                            : undefined
+                                    }
+                                    analyticsEventArgs={{
+                                        context: arbeidsflate,
+                                        category: AnalyticsCategory.Header,
+                                        action: 'varsel-lenke',
+                                        label: varsel.url,
+                                    }}
+                                >
+                                    {currentConfig.lenketekst}
+                                </LenkeMedSporing>
+                            </div>
+                        </div>
+                    </li>
+                );
+            })}
         </ul>
-    )
+    );
 };
