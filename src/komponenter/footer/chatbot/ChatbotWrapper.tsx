143eaf50a11dafaaa4586255d2ff2e827323cbf6
thomaslindstr_m <t@hom.as>
2020-12-10 13:24:25 +0100

    Only enable chatbot if configured to

--- src/komponenter/footer/chatbot/ChatbotWrapper.tsx
+++ src/komponenter/footer/chatbot/ChatbotWrapper.tsx
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { AppState } from 'store/reducers';
 import { verifyWindowObj } from 'utils/Environment';
 import { logAmplitudeEvent } from 'utils/amplitude';
 
 // Prevents SSR crash
 const Chatbot = verifyWindowObj() ? require('@navikt/nav-chatbot') : () => null;
 
+const stateSelector = (state: AppState) => ({
+    isChatbotEnabled: state.environment.PARAMS.CHATBOT
+});
+
 export const ChatbotWrapper = ({...properties}: any) => {
+    const {isChatbotEnabled} = useSelector(stateSelector);
     const [isMounted, setIsMounted] = useState(false);
 
     useEffect(() => {
         const chatbotVersion122IsMounted = document.getElementsByClassName('gxKraP').length > 0;
 
-        if (!chatbotVersion122IsMounted) {
+        if (!chatbotVersion122IsMounted && isChatbotEnabled) {
             setIsMounted(true);
         }
     }, []);
