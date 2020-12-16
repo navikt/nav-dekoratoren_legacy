498dbf738dc53eb430a0621a23db230780ff40a1
anders <anders@nomerstad.com>
2020-12-16 12:48:37 +0100

    Lukker menyer ved klikk på søk og mobil til forsiden lenke. UU-fiks på uleste varsler (øker kontrast og font-size)

--- src/komponenter/header/header-regular/mobil/meny/innhold/elementer/MenyIngress.tsx
+++ src/komponenter/header/header-regular/mobil/meny/innhold/elementer/MenyIngress.tsx
@@ -1,28 +1,15 @@
 import React from 'react';
 import Undertittel from 'nav-frontend-typografi/lib/undertittel';
-import Lenke from 'nav-frontend-lenker';
 import { arbeidsflateLenker } from 'komponenter/common/arbeidsflate-lenker/arbeidsflate-lenker';
 import { useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
+import { LenkeMedSporing } from 'komponenter/common/lenke-med-sporing/LenkeMedSporing';
 
-const MenyIngress = ({
-    className,
-    inputext,
-}: {
-    className: string;
-    inputext: string;
-}) => {
+const MenyIngress = ({ className, inputext }: { className: string; inputext: string }) => {
     const { XP_BASE_URL } = useSelector((state: AppState) => state.environment);
-    const lenke = arbeidsflateLenker(XP_BASE_URL).filter(
-        (lenke) => lenke.key === inputext
-    );
+    const lenke = arbeidsflateLenker(XP_BASE_URL).filter((lenke) => lenke.key === inputext);
 
-    const textToLowercase = inputext
-        ? inputext
-              .toUpperCase()
-              .charAt(0)
-              .concat(inputext.slice(1).toLowerCase())
-        : '';
+    const textToLowercase = inputext ? inputext.toUpperCase().charAt(0).concat(inputext.slice(1).toLowerCase()) : '';
 
     if (!lenke.length) {
         return null;
@@ -31,9 +18,7 @@ const MenyIngress = ({
     return (
         <div className={className}>
             <Undertittel>{textToLowercase}</Undertittel>
-            <Lenke href={lenke[0].url ? lenke[0].url : 'https://nav.no'}>
-                Til forsiden
-            </Lenke>
+            <LenkeMedSporing href={lenke[0].url ? lenke[0].url : 'https://nav.no'}>Til forsiden</LenkeMedSporing>
         </div>
     );
 };
