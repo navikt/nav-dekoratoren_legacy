b5891cb04e5140b9fe003171b37f9e0655c082ce
Morten Jansrud <morten.jansrud@nav.no>
2021-01-12 12:08:26 +0100

    Minimize css

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,5 +1,6 @@
 const moment = require('moment');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
@@ -139,6 +140,13 @@ const browserConfig = {
             matchZones: 'Europe/Oslo',
         }),
     ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            `...`,
+            new CssMinimizerPlugin(),
+        ],
+    },
 };
 
 module.exports = browserConfig;
