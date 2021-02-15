8b73a196d07ee9350b9ac4fd223a3c240d6b949a
anders <anders@nomerstad.com>
2021-02-15 11:20:38 +0100

    Sporer parametre med amplitude

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -1,9 +1,19 @@
 import { verifyWindowObj } from 'utils/Environment';
+import { Params } from 'store/reducers/environment-duck';
+import { initialState } from 'store/reducers/environment-duck';
+
+const defaultParams = initialState.PARAMS;
+
+// const getNonDefaultParams = (params: Params) => {
+//     return Object.keys(params).reduce((acc, key) => {
+//         return params[key as keyof Params] !== defaultParams[key as keyof Params] ? { ...acc } : acc;
+//     }, {});
+// };
 
 // Hindrer crash ved server-side kjøring (amplitude.js fungerer kun i browser)
 const amplitude = verifyWindowObj() ? require('amplitude-js') : () => null;
 
-export const initAmplitude = () => {
+export const initAmplitude = (params: Params) => {
     if (amplitude) {
         amplitude.getInstance().init('default', '', {
             apiEndpoint: 'amplitude.nav.no/collect-auto',
@@ -14,6 +24,7 @@ export const initAmplitude = () => {
         });
         logAmplitudeEvent('sidevisning', {
             sidetittel: document.title,
+            parametre: params,
         });
     }
 };
