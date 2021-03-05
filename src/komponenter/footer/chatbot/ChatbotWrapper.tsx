466a5a6c4c77d029b541be6e639a746c0bf202b4
thomaslindstr_m <t@hom.as>
2021-03-05 14:01:37 +0100

    Add more staging subdomains

--- src/komponenter/footer/chatbot/ChatbotWrapper.tsx
+++ src/komponenter/footer/chatbot/ChatbotWrapper.tsx
@@ -6,7 +6,18 @@ import { logAmplitudeEvent } from 'utils/amplitude';
 
 // Prevents SSR crash
 const Chatbot = verifyWindowObj() ? require('@navikt/nav-chatbot') : () => null;
-const stagingUrlHosts = ['www-q0.nav.no', 'www-q1.nav.no', 'www-q6.nav.no', 'dekoratoren.dev.nav.no'];
+const stagingUrlHosts = [
+    'www-q0.nav.no',
+    'www-q1.nav.no',
+    'www-q2.nav.no',
+    'www-q3.nav.no',
+    'www-q4.nav.no',
+    'www-q5.nav.no',
+    'www-q6.nav.no',
+    'dekoratoren.dev.nav.no',
+    'dev.nav.no',
+    'localhost',
+];
 
 const stateSelector = (state: AppState) => ({
     isChatbotEnabled: state.environment.PARAMS.CHATBOT,
@@ -21,11 +32,8 @@ export const ChatbotWrapper = ({ ...properties }: any) => {
         setIsMounted(!chatbotVersion122IsMounted && isChatbotEnabled);
     }, [isChatbotEnabled]);
 
-    const isStaging = verifyWindowObj() 
-        && stagingUrlHosts.includes(window.location.host);
-    const boostApiUrlBase = isStaging
-        ? 'https://staging-nav.boost.ai/api/chat/v2'
-        : 'https://nav.boost.ai/api/chat/v2';
+    const isStaging = verifyWindowObj() && stagingUrlHosts.includes(window.location.hostname);
+    const boostApiUrlBase = isStaging ? 'https://staging-nav.boost.ai/api/chat/v2' : 'https://nav.boost.ai/api/chat/v2';
     const actionFilters = isStaging ? ['NAV_TEST'] : undefined;
 
     return isMounted ? (
