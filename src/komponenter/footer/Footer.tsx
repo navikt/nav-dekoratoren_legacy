e3b5bd66975ae559c8d8e8b8100e8b38770a203f
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-06-04 17:25:26 +0200

    lagt til utloggingsvarsel i footer og lagt til unntak i postcss-prefix-config

--- src/komponenter/footer/Footer.tsx
+++ src/komponenter/footer/Footer.tsx
@@ -4,11 +4,13 @@ import { AppState } from 'store/reducers';
 import SimpleFooter from './footer-simple/FooterSimple';
 import FooterRegular from './footer-regular/FooterRegular';
 import './Footer.less';
+import Utloggingsvarsel from '../common/utloggingsvarsel/Utloggingsvarsel';
 
 const Footer = () => {
     const { PARAMS } = useSelector((state: AppState) => state.environment);
     return (
         <div className={'decorator-wrapper'}>
+            <Utloggingsvarsel />
             <footer className="sitefooter">
                 {PARAMS.SIMPLE || PARAMS.SIMPLE_FOOTER ? (
                     <SimpleFooter />
