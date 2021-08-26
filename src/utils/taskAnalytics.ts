1539d2645fc6b2d6a79cda689c490138e35df193
Terje Karlsen <terjeofnorway@gmail.com>
2021-08-26 09:51:41 +0200

    fikser feil med flere babel-polyfill-instanser

--- src/utils/taskAnalytics.ts
+++ src/utils/taskAnalytics.ts
@@ -1,4 +1,4 @@
-import 'babel-polyfill';
+import 'regenerator-runtime/runtime';
 
 import { Params } from 'store/reducers/environment-duck';
 import { loadExternalScript } from './external-scripts';
