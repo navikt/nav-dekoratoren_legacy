c4a007de962be0b1532618640310203dc21f45e9
Anders Bråthen Nomerstad <anders@nomerstad>
2022-01-06 15:43:58 +0100

    Refactor driftsmeldinger caching

--- src/server/api-endpoints/driftsmeldinger.ts
+++ src/server/api-endpoints/driftsmeldinger.ts
@@ -9,26 +9,38 @@ const cacheKey = 'driftsmeldinger-cache';
 
 const cache = new NodeCache({
     stdTTL: tenSeconds,
+    deleteOnExpire: false,
 });
+
+const refreshCache = () => {
+    fetch(driftsmeldingerUrl)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                throw new Error(`${response.status} - ${response.statusText}`);
+            }
+        })
+        .then((json) => {
+            cache.set(cacheKey, json);
+        })
+        .catch((e) => {
+            console.error(`Failed to fetch from driftsmeldinger service - ${e}`);
+            const prevCache = cache.get(cacheKey) || [];
+            cache.set(cacheKey, prevCache);
+        });
+};
+
+cache.on('expired', refreshCache);
+
+refreshCache();
 
 export const getDriftsmeldingerHandler: RequestHandler = async (req, res) => {
     const cached = cache.get(cacheKey);
-    if (cached) {
-        return res.status(200).send(cached);
+
+    if (!cached) {
+        return res.status(500).send('Unknown server error - driftsmeldinger not available');
     }
 
-    try {
-        const response = await fetch(driftsmeldingerUrl);
-
-        if (response.status !== 200) {
-            return res.status(response.status).send(`Failed to fetch driftsmeldinger - ${response.statusText}`);
-        }
-
-        const json = await response.json();
-        cache.set(cacheKey, json);
-
-        return res.status(200).send(json);
-    } catch (e) {
-        return res.status(500).send(`Failed to fetch driftsmeldinger - ${e}`);
-    }
+    return res.status(200).send(cached);
 };
