584a780e192d0b79495691850b024b2f445ea700
Anders Bråthen Nomerstad <anders@nomerstad>
2022-01-06 14:49:45 +0100

    Cacher driftsmeldinger

--- src/server/api-endpoints/driftsmeldinger.ts
+++ src/server/api-endpoints/driftsmeldinger.ts
@@ -1,15 +1,34 @@
 import fetch from 'node-fetch';
 import { RequestHandler } from 'express';
+import NodeCache from 'node-cache';
+import { tenSeconds } from '../utils';
 
 const driftsmeldingerUrl = `${process.env.API_XP_SERVICES_URL}/no.nav.navno/driftsmeldinger`;
 
-export const getDriftsmeldingerHandler: RequestHandler = async (req, res) => {
-    const response = await fetch(driftsmeldingerUrl);
+const cacheKey = 'driftsmeldinger-cache';
 
-    if (response.status === 200) {
-        const json = await response.json();
-        return res.status(200).send(json);
+const cache = new NodeCache({
+    stdTTL: tenSeconds,
+});
+
+export const getDriftsmeldingerHandler: RequestHandler = async (req, res) => {
+    const cached = cache.get(cacheKey);
+    if (cached) {
+        return res.status(200).send(cached);
     }
 
-    return res.status(response.status).send(response.statusText);
+    try {
+        const response = await fetch(driftsmeldingerUrl);
+
+        if (response.status !== 200) {
+            return res.status(response.status).send(`Failed to fetch driftsmeldinger - ${response.statusText}`);
+        }
+
+        const json = await response.json();
+        cache.set(cacheKey, json);
+
+        return res.status(200).send(json);
+    } catch (e) {
+        return res.status(500).send(`Failed to fetch driftsmeldinger - ${e}`);
+    }
 };
