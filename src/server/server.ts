2c09ba8edd3389b43853e460e7debf4c409a47d5
Anders Bråthen Nomerstad <anders@nomerstad>
2022-01-17 12:49:33 +0100

    Setter CORP-header

--- src/server/server.ts
+++ src/server/server.ts
@@ -43,6 +43,7 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', req.get('origin'));
         res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
         res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Cross-Origin-Resource-Policy', 'same-site');
 
         const requestHeaders = req.header('Access-Control-Request-Headers');
         if (requestHeaders) {
