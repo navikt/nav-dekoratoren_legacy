7b3091eca34f69dc5b16fea09a7fc89ad2ea13a4
Morten Mikalsen <43532815+mamikals@users.noreply.github.com>
2022-05-11 11:58:24 +0200

    Add temporary scratch org CORS whitelist

--- src/server/server.ts
+++ src/server/server.ts
@@ -40,6 +40,7 @@ const corsWhitelist = [
     'navdialog.cs106.force.com',
     'navdialog.cs108.force.com',
     'navdialog.cs162.force.com',
+    'sandbox-fun-force-1167-180b24b1dc3.cs100.force.com',
 ];
 
 const isAllowedDomain = (origin?: string) => origin && corsWhitelist.some((domain) => origin.endsWith(domain));
