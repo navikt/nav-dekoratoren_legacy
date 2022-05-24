562da98eebaba24813557e928e3246c9e2e17f9e
Eivind Skifjeld <eivind.skifjeld@gmail.com>
2022-05-24 14:19:58 +0200

    Fjernet at label er optiona

--- src/komponenter/header/header-regular/common/sok/sok-innhold/SokInput.tsx
+++ src/komponenter/header/header-regular/common/sok/sok-innhold/SokInput.tsx
@@ -1,36 +1,30 @@
 import Tekst from 'tekster/finn-tekst';
-import { finnTekst } from 'tekster/finn-tekst';
 import { Input, Label } from 'nav-frontend-skjema';
 import SokKnapper from './SokKnapper';
 import React from 'react';
 import { Locale } from 'store/reducers/language-duck';
 import { verifyWindowObj } from 'utils/Environment';
 import './SokInput.less';
-
 type Props = {
     className: string;
     language: Locale;
     writtenInput: string;
     onChange: (value: string) => void;
     onReset: () => void;
-    id?: string;
+    id: string;
 };
-
 export const SokInput = (props: Props) => {
     const { onChange, onReset } = props;
-    const { language, writtenInput, className, id } = props;
-
+    const { writtenInput, className, id } = props;
     // Only set the input value in the browser, to prevent execution-order
     // dependent SSR warnings under certain circumstances
     const inputValue = verifyWindowObj() ? writtenInput || '' : undefined;
 
     return (
         <>
-            <div>
-                <Label className="sok-input-resultat" htmlFor={'sok-input__tittel'}>
-                    <Tekst id={finnTekst('sok-knapp-sokefelt', language)} />
-                </Label>
-            </div>
+            <Label className="sok-input-resultat" htmlFor={id}>
+                <Tekst id={'sok-knapp-sokefelt'} />
+            </Label>
             <div className="sok-input-container">
                 <Input
                     id={id}
@@ -44,5 +38,4 @@ export const SokInput = (props: Props) => {
         </>
     );
 };
-
 export default SokInput;
