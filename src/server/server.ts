461e5a904bf593e2344093830d77fca885e520fd
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-07-11 11:04:34 +0200

    Håndterer rewrite for client.js/css dersom requesten har query eller hash i url

--- src/server/server.ts
+++ src/server/server.ts
@@ -129,7 +129,7 @@ app.get(`${appBasePath}/isReady`, (req, res) => res.sendStatus(200));
 app.use(
     createPaths('/'),
     // Strip cache buster segment from client.css/js files
-    rewrite('*/client:buildId.(css|js)', '$1/client.$3'),
+    rewrite('*/client:buildId.(css|js)*', '$1/client.$3'),
     express.static(buildPath, {
         setHeaders: (res: Response) => {
             if (isProduction) {
