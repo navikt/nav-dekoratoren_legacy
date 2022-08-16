b9dabca1769abef005943cfb750163d188d73a20
Bjørn Harbo Dahle <bdahle@hey.com>
2022-08-16 14:35:18 +0200

    LinkLoader til scss

--- src/komponenter/common/content-loaders/LinkLoader.tsx
+++ src/komponenter/common/content-loaders/LinkLoader.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
-import './ContentLoaders.less';
+import style from './LinkLoader.module.scss';
 
 export const LinkLoader = ({ id }: { id: string }) => (
-    <div className={'content-loader'}>
+    <div className={style.linkLoader}>
         <ContentLoader uniqueKey={id} speed={1} width={225} height={18}>
             <rect x="0" y="4" rx="4" ry="4" width="225" height="16" />
         </ContentLoader>
@@ -11,7 +11,7 @@ export const LinkLoader = ({ id }: { id: string }) => (
 );
 
 export const LinksLoader = ({ id }: { id: string }) => (
-    <div className={'content-loader'}>
+    <div className={style.linkLoader}>
         <ContentLoader speed={1} uniqueKey={id} backgroundColor={'#005077'} foregroundColor={'#196da2'}>
             <rect x="0" y="5" rx="4" ry="4" width="100" height="10" />
             <rect x="0" y="50" rx="4" ry="4" width="135" height="10" />
