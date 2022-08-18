cbb1856caa334819ceeae1ca3bdc89e6c6075bf2
Bjørn Harbo Dahle <bdahle@hey.com>
2022-08-18 09:08:18 +0200

    ArbeidsflateLenkepanel mangler hvit tekst

--- src/komponenter/common/arbeidsflate-lenkepanel/ArbeidsflateLenkepanel.tsx
+++ src/komponenter/common/arbeidsflate-lenkepanel/ArbeidsflateLenkepanel.tsx
@@ -5,7 +5,6 @@ import { useCookies } from 'react-cookie';
 
 import Tekst from 'tekster/finn-tekst';
 import { analyticsEvent } from 'utils/analytics/analytics';
-import BEMHelper from 'utils/bem';
 import { ArbeidsflateLenke } from 'komponenter/common/arbeidsflate-lenker/arbeidsflate-lenker';
 import { Locale } from 'store/reducers/language-duck';
 import { CookieName, cookieOptions } from '../../../server/cookieSettings';
@@ -16,10 +15,7 @@ import { AnalyticsEventArgs } from 'utils/analytics/analytics';
 import { lukkAlleDropdowns } from 'store/reducers/dropdown-toggle-duck';
 import classNames from 'classnames';
 import { Next } from '@navikt/ds-icons';
-
-import './ArbeidsflateLenkepanel.less';
-
-const cls = BEMHelper('arbeidsflate-lenkepanel');
+import style from './ArbeidsflateLenkepanel.module.scss';
 
 interface Props {
     lenke: ArbeidsflateLenke;
@@ -48,9 +44,10 @@ const ArbeidsflateLenkepanel = ({
             href={lenke.url}
             id={id}
             className={classNames(
-                cls.className,
-                inverted ? cls.modifier('inverted') : undefined,
-                enableCompactView ? cls.modifier('compact') : undefined
+                style.arbeidsflateLenkepanel,
+                'arbeidsflate-lenkepanel',
+                inverted ? style.inverted : undefined,
+                enableCompactView ? style.compact : undefined
             )}
             onClick={(event) => {
                 setCookie(CookieName.DECORATOR_CONTEXT, lenke.key, cookieOptions);
@@ -64,12 +61,12 @@ const ArbeidsflateLenkepanel = ({
             border={true}
         >
             <div>
-                <LinkPanel.Title className={cls.element('text')}>
-                    <Next className={'compact-chevron'} />
+                <LinkPanel.Title className={style.text}>
+                    <Next className="compact-chevron" />
                     <Tekst id={lenke.lenkeTekstId} />
                 </LinkPanel.Title>
                 {withDescription && (
-                    <LinkPanel.Description className={cls.element('text')}>
+                    <LinkPanel.Description className={style.text}>
                         {finnTekst(lenke.stikkordId, language)}
                     </LinkPanel.Description>
                 )}
