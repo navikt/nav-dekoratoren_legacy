f48f3ad34433cf29f8372b3e22a76935dd8c3baf
Bjørn Harbo Dahle <bdahle@hey.com>
2022-08-18 09:28:49 +0200

    fjern relatert kode

--- src/komponenter/footer/Footer.tsx
+++ src/komponenter/footer/Footer.tsx
@@ -3,13 +3,11 @@ import { useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
 import SimpleFooter from './footer-simple/FooterSimple';
 import FooterRegular from './footer-regular/FooterRegular';
-import Utloggingsvarsel from '../common/utloggingsvarsel/Utloggingsvarsel';
 
 const Footer = () => {
     const { PARAMS } = useSelector((state: AppState) => state.environment);
     return (
         <div className="decorator-wrapper">
-            <Utloggingsvarsel />
             <footer className="sitefooter">
                 {PARAMS.SIMPLE || PARAMS.SIMPLE_FOOTER ? <SimpleFooter /> : <FooterRegular />}
             </footer>
