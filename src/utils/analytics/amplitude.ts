922d914bc9ad9ed1644db2c7f127dcc5b32b5796
perolavm <per.olav.mariussen@nav.no>
2022-08-26 11:39:52 +0200

    Platform på alle Amplitude events

--- src/utils/analytics/amplitude.ts
+++ src/utils/analytics/amplitude.ts
@@ -20,7 +20,6 @@ export const initAmplitude = () => {
 export const logPageView = (params: Params, authState: InnloggingsstatusState) => {
     logAmplitudeEvent('besøk', {
         sidetittel: document.title,
-        platform: window.location.toString(),
         innlogging: authState.data.securityLevel ?? false,
         parametre: {
             ...params,
@@ -35,6 +34,7 @@ export const logPageView = (params: Params, authState: InnloggingsstatusState) =
 export const logAmplitudeEvent = (eventName: string, data?: any): Promise<any> => {
     return new Promise(function (resolve: any) {
         const eventData = data || {};
+        eventData.platform = window.location.toString();
         eventData.origin = 'dekoratøren';
         eventData.originVersion = 'unknown';
 
