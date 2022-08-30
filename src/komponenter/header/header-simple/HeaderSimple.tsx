9b783675c59180d21ae705fea81b0a1dbeebf717
Bjørn Harbo Dahle <bdahle@hey.com>
2022-08-30 13:21:24 +0200

    HeaderSimple til scss

--- src/komponenter/header/header-simple/HeaderSimple.tsx
+++ src/komponenter/header/header-simple/HeaderSimple.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import BEMHelper from 'utils/bem';
 import NavLogoLenke from 'komponenter/common/nav-logo/NavLogoLenke';
 import Navn from './navn/Navn';
 import LoggInnKnapp from 'komponenter/header/header-regular/common/logg-inn/LoggInnKnapp';
@@ -8,10 +7,9 @@ import { AnalyticsCategory } from 'utils/analytics/analytics';
 import Logo from 'ikoner/meny/nav-logo-black.svg';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../../store/reducers';
-import './HeaderSimple.less';
-import { Status } from '../../../api/api';
+import style from './HeaderSimple.module.scss';
 
-const cls = BEMHelper('simple-header');
+import { Status } from '../../../api/api';
 
 export const HeaderSimple = () => {
     const { innloggingsstatus } = useSelector((state: AppState) => ({
@@ -20,8 +18,8 @@ export const HeaderSimple = () => {
 
     return (
         <Sticky>
-            <div className={cls.className}>
-                <div className={cls.element('content')}>
+            <div className={style.simpleHeader}>
+                <div className={style.content}>
                     <NavLogoLenke
                         analyticsEventArgs={{
                             category: AnalyticsCategory.Header,
@@ -29,7 +27,7 @@ export const HeaderSimple = () => {
                         }}
                         ikon={Logo}
                     />
-                    <div className={cls.element('right')}>
+                    <div className={style.right}>
                         {innloggingsstatus === Status.OK && (
                             <>
                                 <Navn />
