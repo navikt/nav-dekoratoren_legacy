4b4ba13edc0455a9a8c3dcdfc27608144afcff65
Bjørn Harbo Dahle <bdahle@hey.com>
2022-09-05 13:29:33 +0200

    SokIkon til scss

--- src/komponenter/header/header-regular/common/sok/sok-ikon/SokIkon.tsx
+++ src/komponenter/header/header-regular/common/sok/sok-ikon/SokIkon.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import BEMHelper from 'utils/bem';
-import './SokIkon.less';
+import style from './SokIkon.module.scss';
 
 type Props = {
     isOpen: boolean;
 };
 
 export const SokIkon = ({ isOpen }: Props) => {
-    const cls = BEMHelper('sok-ikon');
-
     return (
-        <span className={`${cls.className}${isOpen ? ` ${cls.className}--open` : ''}`}>
-            <span className={cls.element('circle', isOpen ? 'open' : '')} />
-            <span className={cls.element('line', isOpen ? 'open' : '')} />
-            <span className={cls.element('line-x', isOpen ? 'open' : '')} />
+        <span className={style.sokIkon}>
+            <span className={`${style.circle} ${isOpen ? style.circleOpen : ''}`} />
+            <span className={`${style.line} ${isOpen ? style.lineOpen : ''}`} />
+            <span className={`${style.lineX} ${isOpen ? style.lineXOpen : ''}`} />
         </span>
     );
 };
