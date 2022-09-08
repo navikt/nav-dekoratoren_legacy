b3e6da1771015eab935bde2555c746791b451773
Bjørn Harbo Dahle <bdahle@hey.com>
2022-09-08 13:34:33 +0200

    VarselIkon til scss

--- src/komponenter/header/header-regular/common/varsler/varsler-knapp/varsel-ikon/VarselIkon.tsx
+++ src/komponenter/header/header-regular/common/varsler/varsler-knapp/varsel-ikon/VarselIkon.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import BEMHelper from 'utils/bem';
 import { Bell } from '@navikt/ds-icons';
-
-import './VarselIkon.less';
+import style from './VarselIkon.module.scss';
 
 type Props = {
     antallUleste?: number;
 };
 
 export const VarselIkon = ({ antallUleste = 0 }: Props) => {
-    const cls = BEMHelper('varselbjelle-ikon');
-
     return (
         <>
             <Bell />
-            <div className={cls.element('ulest-sirkel', antallUleste === 0 ? 'hide' : '')}></div>
+            <div className={`${style.ulestSirkel} ${antallUleste === 0 ? style.hide : ''}`}></div>
         </>
     );
 };
