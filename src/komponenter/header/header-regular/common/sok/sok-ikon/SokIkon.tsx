0cce477e6a8c4becc2ea2a9c9faaab8c5b1a0fb0
Bjørn Harbo Dahle <bdahle@hey.com>
2022-09-27 13:12:55 +0200

    fiks farge søkeikoner

--- src/komponenter/header/header-regular/common/sok/sok-ikon/SokIkon.tsx
+++ src/komponenter/header/header-regular/common/sok/sok-ikon/SokIkon.tsx
@@ -6,11 +6,15 @@ type Props = {
 };
 
 export const SokIkon = ({ isOpen }: Props) => {
+    const styleCircleOpen = `${style.circleOpen} sok-ikon__circle--open`;
+    const styleLineOpen = `${style.lineOpen} sok-ikon__line--open`;
+    const styleLineXOpen = `${style.lineXOpen} sok-ikon__line-x--open`;
+
     return (
-        <span className={style.sokIkon}>
-            <span className={`${style.circle} ${isOpen ? style.circleOpen : ''}`} />
-            <span className={`${style.line} ${isOpen ? style.lineOpen : ''}`} />
-            <span className={`${style.lineX} ${isOpen ? style.lineXOpen : ''}`} />
+        <span className={`${style.sokIkon} sok-ikon`}>
+            <span className={`${style.circle} sok-ikon__circle ${isOpen ? styleCircleOpen : ''}`} />
+            <span className={`${style.line} sok-ikon__line ${isOpen ? styleLineOpen : ''}`} />
+            <span className={`${style.lineX} sok-ikon__line-x ${isOpen ? styleLineXOpen : ''}`} />
         </span>
     );
 };
