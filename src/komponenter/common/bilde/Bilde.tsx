4a41c98a108bf92765c13dc6133e5d20f7336efa
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-10-28 18:49:08 +0200

    Logo cache-buster

--- src/komponenter/common/bilde/Bilde.tsx
+++ src/komponenter/common/bilde/Bilde.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
 
-type Props = { asset: string; altText?: string; className?: string; ariaHidden?: boolean };
+type Props = {
+    asset: string;
+    altText?: string;
+    className?: string;
+    ariaHidden?: boolean;
+    cacheBuster?: string;
+};
 
-export const Bilde = (props: Props) => {
+export const Bilde = ({ asset, altText, className, ariaHidden, cacheBuster }: Props) => {
     const { APP_URL } = useSelector((state: AppState) => state.environment);
     return (
         <img
-            src={`${APP_URL}${props.asset}`}
-            alt={props.altText || ''}
-            className={props.className}
-            aria-hidden={props.ariaHidden}
+            src={`${APP_URL}${asset}${cacheBuster ? `?ts=${cacheBuster}` : ''}`}
+            alt={altText || ''}
+            className={className}
+            aria-hidden={ariaHidden}
         />
     );
 };
