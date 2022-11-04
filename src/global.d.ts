a626f50bd4c02421fdbe749f74359eb096741082
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-11-04 13:28:25 +0100

    Type def for env vars

--- src/global.d.ts
+++ src/global.d.ts
@@ -5,8 +5,16 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             ENV: 'prod' | 'dev' | 'localhost';
-            API_DEKORATOREN_URL: string;
             APP_BASE_URL: string;
+            APP_BASE_PATH: string;
+            API_XP_SERVICES_URL: string;
+            API_DEKORATOREN_URL: string;
+            MINSIDE_ARBEIDSGIVER_URL: string;
+            MIN_SIDE_URL: string;
+            LOGIN_URL: string;
+            LOGOUT_URL: string;
+            FEEDBACK_API_URL: string;
+            BUILD_ID: string;
         }
     }
 }
