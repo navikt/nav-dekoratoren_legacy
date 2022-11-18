acc831165dfbe8c02fab8463f20c3d7a486becf5
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2022-11-18 08:50:55 +0100

    Spesifisere typer i typescript.

--- src/utils/sorter.ts
+++ src/utils/sorter.ts
@@ -1,3 +1,4 @@
 import dayjs from 'dayjs';
+import { Varsler } from 'store/reducers/varselinnboks-duck';
 
-export const sortByEventTidspunkt = (a: any, b: any) => (dayjs(a.tidspunkt).isAfter(dayjs(b.tidspunkt)) ? -1 : 1);
+export const sortByEventTidspunkt = (a: Varsler, b: Varsler) => (dayjs(a.tidspunkt).isAfter(dayjs(b.tidspunkt)) ? -1 : 1);
