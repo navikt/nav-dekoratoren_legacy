ea9160d3576933eb3fb4c9af98624ae7fc82caa1
Terje Karlsen <terjeofnorway@gmail.com>
2023-02-02 09:39:45 +0100

    Diverse koderydding

--- src/komponenter/header/common/sprakvelger/SprakVelger.tsx
+++ src/komponenter/header/common/sprakvelger/SprakVelger.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
-import { BodyShort } from '@navikt/ds-react';
-import Globe from 'ikoner/globe.svg';
 
+import { BodyShort } from '@navikt/ds-react';
 import { Expand } from '@navikt/ds-icons';
+import classNames from 'classnames';
+
 import { decoratorLanguageCookie } from '../../Header';
 import { cookieOptions } from '../../../../server/cookieSettings';
 import { AvailableLanguage } from 'store/reducers/language-duck';
@@ -12,14 +13,11 @@ import { useCookies } from 'react-cookie';
 import { useSelector, useStore } from 'react-redux';
 import { AppState } from 'store/reducers';
 import { Bilde } from '../../../common/bilde/Bilde';
+
+import Globe from 'ikoner/globe.svg';
+
 import SprakVelgerItem from './SprakVelgerItem';
 import style from 'komponenter/header/common/sprakvelger/SprakVelger.module.scss';
-import classNames from 'classnames';
-
-export const farger = {
-    navGra20: '#C6C2BF',
-    navBla: '#0067C5',
-};
 
 export type LocaleOption = AvailableLanguage & { label: string };
 
@@ -30,8 +28,8 @@ interface Props {
 export const SprakVelger = (props: Props) => {
     const store = useStore();
     const availableLanguages = props.languages;
-    const [isOpen, setIsOpen] = React.useState(false);
     const { language } = useSelector((state: AppState) => state.language);
+    const [isOpen, setIsOpen] = React.useState(false);
     const [selectedItem, setSelectedItem] = React.useState<LocaleOption | null>(null);
     const [, setCookie] = useCookies([decoratorLanguageCookie]);
     const options = transformOptions(availableLanguages).sort((a, b) => (a.label > b.label ? -1 : 1));
@@ -44,12 +42,11 @@ export const SprakVelger = (props: Props) => {
     }, []);
 
     const onChange = (selected: LocaleOption) => {
-        console.log(selected);
         const { label, ...selectedLanguage } = selected;
         setSelectedItem(selected);
-
         setCookie(decoratorLanguageCookie, selectedLanguage.locale, cookieOptions);
         store.dispatch(languageDuck.actionCreator({ language: selectedLanguage.locale }));
+
         if (selectedLanguage.handleInApp) {
             postMessageToApp('languageSelect', selectedLanguage);
         } else {
@@ -57,7 +54,7 @@ export const SprakVelger = (props: Props) => {
         }
     };
 
-    const handleKeyUp = (e: KeyboardEvent) => {
+    const onKeyUp = (e: KeyboardEvent) => {
         if (e.key === 'Tab') {
             const buttons = document.querySelectorAll(`.${style.sprakvelger} button`);
             const hasButtonFocus = Array.from(buttons).some((button) => button === document.activeElement);
@@ -71,7 +68,7 @@ export const SprakVelger = (props: Props) => {
         }
     };
 
-    const handleMouseClick = (e: MouseEvent) => {
+    const onMouseClick = (e: MouseEvent) => {
         const isClickOutside = !e.composedPath().some((el) => el === document.querySelector(`.${style.sprakvelger}`));
         if (isClickOutside) {
             toggleMenu(false);
@@ -79,17 +76,17 @@ export const SprakVelger = (props: Props) => {
     };
 
     const toggleMenu = (open?: boolean) => {
-        const desireOpen = open !== undefined ? open : !isOpen;
+        const intentionToOpen = open !== undefined ? open : !isOpen;
 
-        if (desireOpen) {
-            window.addEventListener('keyup', handleKeyUp);
-            window.addEventListener('click', handleMouseClick);
+        if (intentionToOpen) {
+            window.addEventListener('keyup', onKeyUp);
+            window.addEventListener('click', onMouseClick);
         } else {
-            window.removeEventListener('keyup', handleKeyUp);
-            window.removeEventListener('click', handleMouseClick);
+            window.removeEventListener('keyup', onKeyUp);
+            window.removeEventListener('click', onMouseClick);
         }
 
-        setIsOpen(desireOpen);
+        setIsOpen(intentionToOpen);
     };
 
     return (
