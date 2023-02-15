02b2949aa2599bea2bfe34c8626f776d8c271d0d
Martin Berglund <martin.berglund@nav.no>
2023-02-15 11:50:56 +0100

    Legge til høyde og bredde på logo

--- src/komponenter/common/bilde/Bilde.tsx
+++ src/komponenter/common/bilde/Bilde.tsx
@@ -6,17 +6,21 @@ type Props = {
     asset: string;
     altText?: string;
     className?: string;
+    width?: number;
+    height?: number;
     ariaHidden?: boolean;
     cacheBuster?: string;
 };
 
-export const Bilde = ({ asset, altText, className, ariaHidden, cacheBuster }: Props) => {
+export const Bilde = ({ asset, altText, className, width, height, ariaHidden, cacheBuster }: Props) => {
     const { APP_URL } = useSelector((state: AppState) => state.environment);
     return (
         <img
             src={`${APP_URL}${asset}${cacheBuster ? `?ts=${cacheBuster}` : ''}`}
             alt={altText || ''}
             className={className}
+            width={width}
+            height={height}
             aria-hidden={ariaHidden}
         />
     );
