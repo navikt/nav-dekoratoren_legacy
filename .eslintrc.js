b2bcaef8474ef8bc192a4ea335bde2cd8913f24f
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-03-13 14:24:09 +0100

    Rename action

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,22 @@
 module.exports = {
     plugins: ['react', '@typescript-eslint'],
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended'
-    ],
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
-    root: true,
     rules: {
         'react/prop-types': 'off',
         '@typescript-eslint/no-unused-vars': [
             'warn',
             {
-                'argsIgnorePattern': '^(_*|req|res|next)$'
-            }
+                argsIgnorePattern: '^(_*|req|res|next)$',
+            },
         ],
         '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-var-requires': 'off'
+        '@typescript-eslint/no-var-requires': 'off',
     },
     env: { browser: true, node: true },
     settings: {
         react: {
-            version: "detect"
-        }
-    }
+            version: 'detect',
+        },
+    },
 };
