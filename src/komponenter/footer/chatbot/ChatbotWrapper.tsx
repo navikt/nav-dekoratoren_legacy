8578e335848208c0ce6d16dba5714214220bf4ee
Morten Mikalsen <morten.mikalsen@hotmail.com>
2023-04-11 14:59:52 +0200

    Chatbot: Open links in new tab

--- src/komponenter/footer/chatbot/ChatbotWrapper.tsx
+++ src/komponenter/footer/chatbot/ChatbotWrapper.tsx
@@ -57,6 +57,7 @@ export const ChatbotWrapper = () => {
                 settings: {
                     removeRememberedConversationOnChatPanelClose: true,
                     conversationId: cookies[conversationCookieName],
+                    openTextLinksInNewTab: true,
                 },
             },
         };
