82194af29ff11783b4f5ee8616649f236b2b6bd3
Maximilian Øystå Lloyd <me@maxlloyd.no>
2023-06-20 15:45:05 +0200

    Fjernet endringer fra en annen branch

--- src/utils/analytics/amplitude.ts
+++ src/utils/analytics/amplitude.ts
@@ -5,15 +5,12 @@ import { InnloggingsstatusState } from '../../store/reducers/innloggingsstatus-d
 type EventData = Record<string, any>;
 
 export const initAmplitude = () => {
-    const userProps = Object.assign(
-        {
-            skjermbredde: window.screen.width,
-            skjermhoyde: window.screen.height,
-            vindusbredde: window.innerWidth,
-            vindushoyde: window.innerHeight,
-        },
-        window.amplitudeInitialUserProps
-    );
+    const userProps = {
+        skjermbredde: window.screen.width,
+        skjermhoyde: window.screen.height,
+        vindusbredde: window.innerWidth,
+        vindushoyde: window.innerHeight,
+    };
 
     amplitude.getInstance().init('default', '', {
         apiEndpoint: 'amplitude.nav.no/collect-auto',
