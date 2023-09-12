26389429ba3a1485b449a875aa81d897b3964167
Terje Karlsen <terjeofnorway@gmail.com>
2023-09-12 14:03:15 +0200

    Migrerer ds-pakker til v5 og oppdatere Modal-bruk

--- src/komponenter/header/logoutWarning/LogoutWarning.tsx
+++ src/komponenter/header/logoutWarning/LogoutWarning.tsx
@@ -1,4 +1,4 @@
-import { BodyLong, Button, Heading, Modal } from '@navikt/ds-react';
+import { BodyLong, Button, Modal } from '@navikt/ds-react';
 import React, { useEffect } from 'react';
 import { useLoginStatus } from 'utils/hooks/useLoginStatus';
 import { finnTekst } from 'tekster/finn-tekst';
@@ -34,8 +34,6 @@ export const LogoutWarning = () => {
         return null;
     }
 
-    const parent = document.getElementById('logout-warning');
-
     const titleId: LangKey = isSessionExpiring ? 'snart-session-logget-ut-tittel' : 'snart-token-logget-ut-tittel';
     const textBodyId: LangKey = isSessionExpiring ? 'snart-session-logget-ut-body' : 'snart-token-logget-ut-body';
 
@@ -45,16 +43,14 @@ export const LogoutWarning = () => {
         <Modal
             open={isOpen}
             onClose={onCloseHandler}
-            closeButton={false}
-            shouldCloseOnOverlayClick={false}
-            shouldCloseOnEsc={false}
+            header={{
+                heading: finnTekst(titleId, language, minutesToSessionEnd.toString()),
+                closeButton: false,
+            }}
+            onCancel={(e) => e.preventDefault()}
             className={classNames(styles.logoutWarning, isOpen && styles.visible)}
-            parentSelector={parent ? () => parent : undefined}
         >
-            <Modal.Content className={styles.content}>
-                <Heading spacing level="1" size="small">
-                    {finnTekst(titleId, language, minutesToSessionEnd.toString())}
-                </Heading>
+            <Modal.Body className={styles.content}>
                 <BodyLong spacing>{finnTekst(textBodyId, language)}</BodyLong>
                 <div className={styles.buttonWrapper}>
                     {isSessionExpiring && (
@@ -71,7 +67,7 @@ export const LogoutWarning = () => {
                         {finnTekst('logg-ut-knapp', language)}
                     </Button>
                 </div>
-            </Modal.Content>
+            </Modal.Body>
         </Modal>
     );
 };
